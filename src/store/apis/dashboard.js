46f2777e7f183bc7114776e0ca25d989bd70224b
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-08-08 10:36:13 +0530

    error handling in the API

--- src/store/apis/dashboard.js
+++ src/store/apis/dashboard.js
@@ -4,7 +4,7 @@ export const getAllClustersApi = async () => {
   try {
     return await API.get(`/clusters`);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
 
@@ -12,7 +12,7 @@ export const getInitialClusterData = async id => {
   try {
     return await API.get(`/dashboard-insight/${id}/namespaces`);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
 
@@ -22,6 +22,6 @@ export const getNamespaceData = async (clusterId, nameSpaceId) => {
       `/dashboard-insight/${clusterId}/namespaces/${nameSpaceId}`
     );
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
--- src/store/apis/users.js
+++ src/store/apis/users.js
@@ -9,7 +9,7 @@ export const createUserApi = async payload => {
   try {
     return await API.post('/users', payload);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
 
@@ -17,7 +17,7 @@ export const deleteUserApi = async id => {
   try {
     return await API.delete(`/users/${id}`);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
 
@@ -25,13 +25,13 @@ export const getSingleUserData = async id => {
   try {
     return await API.get(`/users/${id}`);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
 export const editUserDataApi = async (id, payload) => {
   try {
     return await API.patch(`/users/${id}`, payload);
   } catch (error) {
-    return error.response.data;
+    return error?.response?.data;
   }
 };
