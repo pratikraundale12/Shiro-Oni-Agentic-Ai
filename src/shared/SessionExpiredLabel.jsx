1db3160513642f340e9e940a035afc7325e21bb4
Vaibhav Sompura <vaibhav.sompura@ksolves.com>
2024-08-19 20:22:04 +0530

    NMS-335 ui changes

--- src/shared/SessionExpiredLabel.jsx
+++ src/shared/SessionExpiredLabel.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { CrossIcon } from '../assets';
 import { KDFM } from '../constants';
 import { AuthenticationSelectors } from '../store';
 
@@ -53,25 +54,7 @@ const SessionExpiredLabel = ({ closeTab }) => {
         <AlertContent>
           <AlertText>{KDFM.TRIAL_EXPIRED_PROMPT(license)}</AlertText>
           <IconContainer onClick={closeTab}>
-            <svg
-              width="33"
-              height="33"
-              viewBox="0 0 33 33"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="16.5"
-                cy="16.5"
-                r="16"
-                fill="white"
-                stroke="#DDE4F0"
-              />
-              <path
-                d="M15.1953 16.1381L10 10.9428L10.9428 10L16.1381 15.1952L21.3334 10L22.2762 10.9428L17.0809 16.1381L22.2762 21.3333L21.3334 22.2762L16.1381 17.0809L10.9428 22.2762L10 21.3333L15.1953 16.1381Z"
-                fill="black"
-              />
-            </svg>
+            <CrossIcon width={24} height={24} />
           </IconContainer>
         </AlertContent>
       </FloatingAlertBox>
