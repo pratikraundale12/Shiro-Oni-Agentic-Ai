4dc056d6aab69ee5d8e5ef77385af8d057f7995f
Vaibhav Sompura <vaibhav.sompura@ksolves.com>
2024-08-21 12:52:57 +0530

    NMS-345 summary page url alignment fix

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -149,8 +149,11 @@ const SummaryDetailsPtag = styled.h4`
 
   & > div {
     display: flex;
-    align-items: center;
     gap: 0.5rem;
+
+    & .summary-clipboard {
+      margin-top: -0.5rem;
+    }
   }
 
   & span {
@@ -479,6 +482,7 @@ const Summary = () => {
                       <div>
                         <span>{checkDestCluster.registryUrl}</span>
                         <CopyToClipboard
+                          className="summary-clipboard"
                           copyItem={checkDestCluster.registryUrl}
                         />
                       </div>
@@ -493,7 +497,10 @@ const Summary = () => {
                     <SummaryDetailsPtag className="mb-0">
                       <div>
                         <span>{checkDestCluster.nifiUrl}</span>
-                        <CopyToClipboard copyItem={checkDestCluster.nifiUrl} />
+                        <CopyToClipboard
+                          className="summary-clipboard"
+                          copyItem={checkDestCluster.nifiUrl}
+                        />
                       </div>
                     </SummaryDetailsPtag>
                   </div>
