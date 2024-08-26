11aacbc6cb2c9aefe37f377cde8f0de1fd7132e6
Vaibhav Sompura <vaibhav.sompura@ksolves.com>
2024-08-26 15:07:00 +0530

    Date Picker styling

--- src/shared/FormInputs/components/DateSelector/index.js
+++ src/shared/FormInputs/components/DateSelector/index.js
@@ -1,13 +1,17 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { CalendarIcon, DownArrowIcon } from '../../../../assets';
 
 const Container = styled.div`
   position: relative;
   width: 100%;
   margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 
   path {
     fill: ${props => props.theme.colors.darkGrey1};
@@ -25,12 +29,72 @@ const Container = styled.div`
     color: ${props => props.theme.colors.error};
     font-size: 1rem;
   }
-  div > div > input {
-    width: 100%;
+
+  & .date-picker-icon-wrapper {
+    display: flex;
+    align-items: center;
+    position: relative;
+    border-radius: 4px;
+
+    & path {
+      fill: ${props => props.theme.colors.darker};
+    }
   }
-  // div {
-  //   width: 100%;
-  // }
+
+  & .date-picker-icon {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    bottom: 2px;
+    z-index: 1;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    padding: 13px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${props => props.theme.colors.lightGrey};
+
+    &.arrow-down {
+      right: 2px;
+      left: auto;
+      background-color: ${props => props.theme.colors.white};
+    }
+  }
+
+  & .date-picker-wrapper {
+    display: flex;
+    flex: 1;
+
+    & input {
+      border: 1px solid ${props => props.theme.colors.border};
+      border-radius: 4px;
+
+      &:focus-visible {
+        outline: none;
+      }
+
+      &:hover {
+        border: 1px solid ${props => props.theme.colors.darker};
+      }
+
+      &:focus {
+        border: 1px solid ${props => props.theme.colors.darker};
+      }
+    }
+  }
+`;
+
+const StyledDatePicker = styled(DatePicker)`
+  width: 100%;
+  height: 54px;
+  padding-left: 3.5rem;
+
+  &::placeholder {
+    color: ${props => props.theme.colors.grey};
+    font-family: ${props => props.theme.fontNato};
+    font-size: 14px;
+  }
 `;
 
 const DateTimeInput = ({
@@ -49,18 +113,26 @@ const DateTimeInput = ({
           {required && <span className="required">&nbsp;*</span>}
         </label>
       )}
-      <br />
-      <DatePicker
-        selected={startDate}
-        onChange={date => setStartDate(date)}
-        showTimeSelect
-        timeFormat="HH:mm"
-        timeIntervals={15}
-        timeCaption="time"
-        dateFormat="MMMM d, yyyy h:mm aa"
-        control={control}
-        errors={errors}
-      />
+      <div className="date-picker-icon-wrapper">
+        <div className="date-picker-icon">
+          <CalendarIcon />
+        </div>
+        <StyledDatePicker
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          timeCaption="time"
+          dateFormat="MMMM d, yyyy h:mm aa"
+          wrapperClassName="date-picker-wrapper"
+          control={control}
+          errors={errors}
+        />
+        <div className="date-picker-icon arrow-down">
+          <DownArrowIcon />
+        </div>
+      </div>
     </Container>
   );
 };
