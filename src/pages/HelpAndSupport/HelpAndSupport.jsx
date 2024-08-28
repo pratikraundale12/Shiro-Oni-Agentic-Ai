b8663c965f4765e718cd77b8f871bbfb105c1c2f
Neha Yadav <neha.yadav@ksolves.com>
2024-08-28 21:19:17 +0530

    everything is working fine

--- src/pages/HelpAndSupport/HelpAndSupport.jsx
+++ src/pages/HelpAndSupport/HelpAndSupport.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '../../shared';
-import { PlusIcon, SmallSearchIcon } from '../../assets';
+import { NoDataIcon, PlusIcon, SmallSearchIcon } from '../../assets';
 import { MinusIcon } from '../../assets/Icons/MinusIcon';
 import dashboardErrorVideo from '../../assets/videos/DashboardError.mp4';
 import dashboardFlowMetricsVideo from '../../assets/videos/DashboardFlowMetrics.mp4';
@@ -70,6 +70,15 @@ const FAQHeading = styled.div`
   margin-top: 30px;
 `;
 
+const BottonHeading = styled.div`
+  font-family: Noto Sans;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 21.85px;
+  text-align: left;
+  margin-top: 30px;
+`;
+
 const AccordionItem = styled.div`
   margin-bottom: 1rem;
   border-bottom: 1px solid #b9c3d3;
@@ -116,11 +125,13 @@ const Title = styled.div`
   font-weight: 700;
   line-height: 22.06px;
   text-align: left;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+  color: #444445;
 `;
 const SearchContainer = styled.div`
   position: relative;
-
+  margin-left: auto;
+  flex: 1;
   svg {
     position: absolute;
     top: 50%;
@@ -143,68 +154,113 @@ const Search = styled.input`
     outline: none;
   }
 `;
+
+const NoDataText = styled.div`
+  color: ${props => props.theme.colors.lightGrey3};
+  font-family: ${props => props.theme.fontNato};
+  font-size: 28px;
+  font-weight: 600;
+  text-align: center;
+`;
+
 export const HelpAndSupport = () => {
   const [activeTab, setActiveTab] = useState('FAQs');
   const [openFaqIndex, setOpenFaqIndex] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const faqs = [
     {
       question: 'What is Data Flow Manager?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'A Data Flow Manager is a tool that controls and monitors the movement and processing of data between systems or applications, ensuring efficient and accurate data handling.',
     },
     {
       question: 'What is the purpose of the DFM?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'The purpose of the DFM (Design for Manufacturing) is to optimize the design of a product to make it easier and more cost-effective to manufacture. It focuses on simplifying the design, reducing production costs, and improving the overall efficiency of the manufacturing process.',
     },
     {
       question: 'What is namespace?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'A namespace is a container that holds a set of identifiers (names) and allows them to be organized and distinguished from other identifiers. It helps avoid naming conflicts by providing a scope in which names can be defined and used, particularly in programming languages and XML.',
     },
     {
-      question: ' How to manage namespace?',
+      question: 'How to manage namespace?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'Define Clear Names: Use descriptive names to avoid conflicts and make code more understandable.',
     },
     {
       question:
-        'How DFM promote the namespace from one cluster to the another cluster?',
+        'How DFM promote the namespace from one cluster to another cluster?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'DFM doesn’t directly promote namespaces between clusters. To do this, export the namespace configuration from one cluster and import it into another using tools like kubectl or Helm charts.',
     },
     {
       question: 'What is cluster and how do we suppose to login?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'A cluster is a group of interconnected servers or nodes that work together to provide a unified computing resource. To log in, use the cluster management interface or command-line tools, such as kubectl for Kubernetes clusters, often requiring authentication with credentials or tokens',
     },
     {
-      question: ' What is the meaning of activate/deactive cluster?',
+      question: 'What is the meaning of activate/deactivate cluster?',
       answer:
-        'Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.',
+        'Activate a cluster means to enable it for use or bring it online, making it operational. Deactivate a cluster means to disable it or take it offline, making it unavailable for use.',
+    },
+    {
+      question: 'How to activate/deactive the cluster?',
+      answer:
+        'Activate with help of button which is available in cluster list and deactivate after delteting',
+    },
+    {
+      question: 'How to login to the cluster?',
+      answer:
+        'To log in to a Kubernetes cluster, use kubectl with a configured kubeconfig file. For cloud services, access the cluster through the provider’s console or CLI tools.',
+    },
+    {
+      question: 'How can I see the nested namespaces?',
+      answer:
+        'Namespaces in Kubernetes are not inherently nested. To view namespaces, use kubectl get namespaces to list all namespaces in a flat structure.',
+    },
+    {
+      question: 'What is flow matrix?',
+      answer:
+        'A flow matrix is a tool used to visualize and analyze the flow of processes, data, or tasks within a system. It helps in understanding the interactions and dependencies between different components or stages.',
+    },
+    {
+      question: 'What is refresh rate how to control?',
+      answer:
+        'The refresh rate is the frequency at which data or content is updated or refreshed. It can be controlled by adjusting settings in the application or system configurations where the data is being displayed or monitored.',
+    },
+    {
+      question: 'Can I change the logo of the application?',
+      answer:
+        'Yes, you can change the logo of an application by updating the logo file in the applications assets or configuration settings. Ensure the new logo is properly referenced in the applications code or settings.',
+    },
+    {
+      question: ' How to switch among clusters?',
+      answer:
+        'To switch between Kubernetes clusters, use kubectl config use-context <context-name> to change the active context. For cloud services, use the providers CLI or management console to select the desired cluster.',
     },
   ];
 
   const videos = [
     {
-      title: 'How do I change my account email?',
+      title: 'How to see the Dashboard Errors?',
       video: dashboardErrorVideo,
     },
     {
-      title: 'How do I change my account email?',
+      title: 'What is the Flow Metrics?',
       video: dashboardFlowMetricsVideo,
     },
     {
-      title: 'How do I change my account email?',
+      title: 'Dashbaord Quick Insight',
       video: dashboardQuickInsight,
     },
     {
-      title: 'How do I change my account email?',
+      title: 'How to Login Through Admin?',
       video: loginToDFMThroughAdmin,
     },
     {
-      title: 'How do I change my account email?',
+      title: 'How do Login Through User?',
       video: loginToDFMThroughUser,
     },
   ];
@@ -212,6 +268,18 @@ export const HelpAndSupport = () => {
   const toggleFaq = index => {
     setOpenFaqIndex(openFaqIndex === index ? null : index);
   };
+
+  const handleSearch = event => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
+
+  const filteredFaqs = faqs.filter(faq =>
+    faq.question.toLowerCase().includes(searchQuery)
+  );
+
+  const filteredVideos = videos.filter(video =>
+    video.title.toLowerCase().includes(searchQuery)
+  );
 
   return (
     <>
@@ -235,52 +303,59 @@ export const HelpAndSupport = () => {
           <>
             <FormContainer>
               <div className="d-flex gap-5">
-                <FAQHeading>Frequently Asked Questions</FAQHeading>
+                <FAQHeading>Frequently Asked Questions?</FAQHeading>
                 <SearchContainer>
                   <SmallSearchIcon
                     width={18}
                     height={18}
                     color={theme.colors.darkGrey1}
                   />
-                  <Search type="search" placeholder="search" />
+                  <Search
+                    type="search"
+                    placeholder="Search Questions ...."
+                    value={searchQuery}
+                    onChange={handleSearch}
+                  />
                 </SearchContainer>
               </div>
 
               <div className="row mt-5">
-                {faqs.map((faq, index) => (
-                  <div
-                    key={index}
-                    className="col-6"
-                    style={{ overflow: 'hidden' }}
-                  >
-                    <AccordionItem>
-                      <AccordionButton onClick={() => toggleFaq(index)}>
-                        <span
-                          style={
-                            openFaqIndex === index ? { color: 'orange' } : null
-                          }
-                        >
-                          {faq.question}
-                        </span>
-                        <div>
+                {filteredFaqs.length > 0 ? (
+                  filteredFaqs.map((faq, index) => (
+                    <div
+                      key={index}
+                      className="col-6"
+                      style={{ overflow: 'hidden' }}
+                    >
+                      <AccordionItem key={index}>
+                        <AccordionButton onClick={() => toggleFaq(index)}>
+                          <span
+                            style={
+                              openFaqIndex === index
+                                ? { color: 'orange' }
+                                : null
+                            }
+                          >
+                            {faq.question}
+                          </span>
                           {openFaqIndex === index ? (
-                            <MinusIcon color="#000000" />
+                            <MinusIcon />
                           ) : (
                             <PlusIcon color="#000000" />
                           )}
-                        </div>
-                      </AccordionButton>
-                      <div
-                        style={{
-                          height: openFaqIndex === index ? 'auto' : '0',
-                          overflow: 'hidden',
-                        }}
-                      >
-                        <AccordionContent>{faq.answer}</AccordionContent>
-                      </div>
-                    </AccordionItem>
-                  </div>
-                ))}
+                        </AccordionButton>
+                        {openFaqIndex === index && (
+                          <AccordionContent>{faq.answer}</AccordionContent>
+                        )}
+                      </AccordionItem>
+                    </div>
+                  ))
+                ) : (
+                  <>
+                    <NoDataIcon width={130} />
+                    <NoDataText>No Data Found!!</NoDataText>
+                  </>
+                )}
               </div>
             </FormContainer>
           </>
@@ -296,34 +371,52 @@ export const HelpAndSupport = () => {
                   height={18}
                   color={theme.colors.darkGrey1}
                 />
-                <Search type="search" placeholder="search" />
+                <Search
+                  type="search"
+                  placeholder="Search Video Tutorials ...."
+                  value={searchQuery}
+                  onChange={handleSearch}
+                />{' '}
               </SearchContainer>
             </div>
             <div className="row mt-3">
-              {videos.map((video, index) => (
-                <div key={index} className="col-4 gap-2">
-                  <video
-                    width="100%"
-                    controls
-                    style={{
-                      borderRadius: '10px',
-                      marginBottom: '5px',
-                      // marginRight: '20px',
-                      paddingRight: '50px',
-                    }}
-                  >
-                    <source src={video.video} type="video/mp4" />
-                  </video>
-                  <Title>How do I change my account email?</Title>
-                </div>
-              ))}
+              {filteredVideos.length > 0 ? (
+                filteredVideos.map((video, index) => (
+                  <div key={index} className="col-4 gap-2">
+                    <video
+                      width="100%"
+                      controls
+                      style={{
+                        borderRadius: '10px',
+                        marginBottom: '10px',
+                        paddingRight: '50px',
+                      }}
+                    >
+                      <source src={video.video} type="video/mp4" />
+                      <track
+                        kind="captions"
+                        srcLang="en"
+                        label="English captions"
+                        src="/path/to/captions.vtt"
+                        default
+                      />
+                    </video>
+                    <Title>{video.title}</Title>
+                  </div>
+                ))
+              ) : (
+                <>
+                  <NoDataIcon width={130} />
+                  <NoDataText>No Data Found!!</NoDataText>
+                </>
+              )}
             </div>
           </FormContainer>
         )}
       </Container>
       <FlexWrapper>
         <div>
-          <FAQHeading>Still have questions?</FAQHeading>
+          <BottonHeading>Still have questions?</BottonHeading>
           <div style={{ marginTop: '15px' }}>
             We aim to respond to all inquiries within 24 hours. Please provide
             as much <br />
@@ -332,7 +425,13 @@ export const HelpAndSupport = () => {
           </div>
         </div>
         <div>
-          <StyledButton>Support@ksolves.com</StyledButton>
+          <StyledButton
+            onClick={() =>
+              (window.location.href = 'mailto:Support@ksolves.com')
+            }
+          >
+            Support@ksolves.com
+          </StyledButton>
         </div>
       </FlexWrapper>
     </>
