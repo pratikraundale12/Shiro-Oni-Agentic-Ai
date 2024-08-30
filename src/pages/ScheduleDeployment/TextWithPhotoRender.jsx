730fb4c52de8cfb477f0faafa00de5a6da69e8f5
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-08-30 18:06:04 +0530

    tooltip issue fix

--- src/pages/ScheduleDeployment/TextWithPhotoRender.jsx
+++ src/pages/ScheduleDeployment/TextWithPhotoRender.jsx
@@ -112,7 +112,7 @@ export const TextWithPhotoRender = ({
                       borderRadius: '8px',
                       padding: '8px 12px',
                       boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
-                      zIndex: 10000,
+                      zIndex: theme.zIndex,
                     }}
                   >
                     <div>{content[index].approver_name}</div>
