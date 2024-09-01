43651c465ad89b4b13b1f083a30b3418b591921e
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-09-01 12:59:47 +0530

    serach issue fix

--- src/pages/Namespaces/ListNamespaces.jsx
+++ src/pages/Namespaces/ListNamespaces.jsx
@@ -8,6 +8,7 @@ import { history } from '../../helpers/history';
 import { Button } from '../../shared';
 import CopyToClipboard from '../../shared/CopyToClipboard';
 import { NamespacesActions } from '../../store';
+import { useGlobalContext } from '../../utils';
 import AuditLog from './AuditLog';
 
 const StyledButton = styled.button`
@@ -31,6 +32,7 @@ export const ListNamespaces = () => {
   const dispatch = useDispatch();
   const [isAuditLogOpen, setIsAuditLogOpen] = useState(false);
   const [selectedRowId, setSelectedRowId] = useState(null);
+  const { setState } = useGlobalContext();
 
   useEffect(() => {
     dispatch(NamespacesActions.resetDeployData());
@@ -63,8 +65,10 @@ export const ListNamespaces = () => {
       label: KDFM.NAMESPACE,
       renderCell: item => (
         <StyledButton
+          key={item.flowId}
           tabIndex="0"
           onClick={() => {
+            setState(prev => ({ ...prev, search: '' }));
             dispatch(NamespacesActions.setFlowPath(item.flowId));
             dispatch(
               NamespacesActions.setSelectedNamespace({
