c3d5b154adf76ee242e7959ac9481b5c541dd96a
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-09-05 16:14:33 +0530

    state sharing while api hit of schedule

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -771,6 +771,7 @@ const Summary = () => {
       <ScheduleNamespaceDeploy
         getScheduleParamerterContext={getScheduleParamerterContext}
         handleScheduleTertiaryButton={handleScheduleTertiaryButton}
+        flowControlState={flowControlButtons}
       />
       <ParameterContext
         isParameterContextOpen={isParameterContextOpen}
