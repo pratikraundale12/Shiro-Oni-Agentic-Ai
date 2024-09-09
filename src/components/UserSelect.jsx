0a01be8ce9ff0672835db861bb95608ac774c0fe
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-09-09 10:49:24 +0530

    code indentation

--- src/components/UserSelect.jsx
+++ src/components/UserSelect.jsx
@@ -14,6 +14,7 @@ import {
 } from '../store';
 
 export const UserSelect = ({ control, errors, name, label, placeholder }) => {
+  
   const dispatch = useDispatch();
   const [selected, setSelected] = useState([]);
   const RoleList = useSelector(RolesSelectors.getRoles);
