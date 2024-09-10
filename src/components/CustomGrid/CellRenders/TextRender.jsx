f5628834244545c458cb7cf14cec684a04a6706c
Neha Yadav <neha.yadav@ksolves.com>
2024-09-10 17:43:50 +0530

    483 fixed

--- src/components/CustomGrid/CellRenders/TextRender.jsx
+++ src/components/CustomGrid/CellRenders/TextRender.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
-
 import styled from 'styled-components';
 
 const TextColor = styled.div`
@@ -29,6 +28,7 @@ export const TextRender = ({
         id={textToRender}
         content={textToRender}
         place={tooltipPlacement}
+        positionStrategy="fixed"
       />
     </TextColor>
   );
