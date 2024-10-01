bc57fa6d1acd7d9cb712e476ed93f11bd6613b5f
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-10-01 12:46:44 +0530

    chnages in the button to add the dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
