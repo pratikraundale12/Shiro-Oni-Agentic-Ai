17880a80b21df7765a5a299cd08e659b9c4b281f
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-11-04 12:20:57 +0530

    changes related to yup validation for add new user

--- src/components/UserManagement/userValidation.jsx
+++ src/components/UserManagement/userValidation.jsx
@@ -16,21 +16,21 @@ export const userSchema = yup.object().shape({
     .matches(EMAIL_REGEX, 'Invalid email address')
     .required('Email is required'),
   role_id: yup.string().required('Role is required'),
-  password: yup
-    .string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[0-9]/, 'Password must contain at least one number')
-    .matches(
-      /[@$!%*?&]/,
-      'Password must contain at least one special character'
-    ),
-  confirm_password: yup
-    .string()
-    .required('Confirm password is required')
-    .oneOf([yup.ref('password')], 'Passwords must match'),
+  // password: yup
+  //   .string()
+  //   .required('Password is required')
+  //   .min(8, 'Password must be at least 8 characters')
+  //   .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+  //   .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  //   .matches(/[0-9]/, 'Password must contain at least one number')
+  //   .matches(
+  //     /[@$!%*?&]/,
+  //     'Password must contain at least one special character'
+  //   ),
+  // confirm_password: yup
+  //   .string()
+  //   .required('Confirm password is required')
+  //   .oneOf([yup.ref('password')], 'Passwords must match'),
   username: yup.string().required('Username is required'),
 });
 
