1dda88176703fb6466aeff2b149339cf38956c62
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-11-08 16:15:19 +0530

    changes

--- src/shared/FormInputs/components/SelectField/index.jsx
+++ src/shared/FormInputs/components/SelectField/index.jsx
@@ -46,7 +46,7 @@ const Container = styled.div`
 
     // Media query for smaller screens
     @media (max-width: 1400px) {
-      max-height: calc(100% - 446px) !important;
+      max-height: calc(100% - 210px) !important;
     }
   }
 `;
