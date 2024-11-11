5410c38749b261fea73ead48eb23ee77e6cd39d3
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-11-11 11:27:04 +0530

    unwanted call of api holded

--- src/pages/LdapConfig/LdapConfig.jsx
+++ src/pages/LdapConfig/LdapConfig.jsx
@@ -34,6 +34,7 @@ import {
 import { SuccessTestModal } from '../Clusters/components/SuccessTestModal';
 import { CreateMapping } from './components/CreateMapping';
 import SelectCellRender from './components/SelectCellRender';
+import { isEmpty } from 'lodash';
 
 const Wrapper = styled.div`
   margin-top: 4px;
@@ -170,7 +171,6 @@ export const LdapConfig = () => {
   const roles = useSelector(RolesSelectors.getRoles);
   const ldapGroup = useSelector(RolesSelectors.getLdapGroup);
   const displayList = useSelector(RolesSelectors.getDiplayData);
-
   const {
     register: registerForm1,
     handleSubmit: handleSubmitForm1,
@@ -323,8 +323,10 @@ export const LdapConfig = () => {
   };
 
   useEffect(() => {
-    dispatch(RolesActions.fetchRoles());
-  }, [dispatch]);
+    if (isEmpty(roles)) {
+      dispatch(RolesActions.fetchRoles());
+    }
+  }, [dispatch, roles]);
 
   useEffect(() => {
     dispatch(RolesActions.displayGroup(true));
