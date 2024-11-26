55245d989a9b9b7e540daaf62b8e1ef8774c717a
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-11-26 17:28:56 +0530

    cluster logout

--- src/components/CustomGrid/CellRenders/EnableClusterRender.jsx
+++ src/components/CustomGrid/CellRenders/EnableClusterRender.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 import styled from 'styled-components';
-import { LoginIcon } from '../../../assets';
-import { CLUSTER_STATUS } from '../../../constants';
-import { AuthenticationActions } from '../../../store';
+import { LoginIcon, LogoutIcon } from '../../../assets';
+import { CLUSTERS_TOKEN, CLUSTER_STATUS } from '../../../constants';
+import {
+  AuthenticationActions,
+  DashboardActions,
+  GridActions,
+  NamespacesActions,
+} from '../../../store';
 import { ClusterLoginModal } from '../../ClusterLoginModal';
 import { IconButton } from './AtionRender';
 
@@ -28,23 +34,62 @@ const EnableClusterText = styled.div`
 export const EnableClusterRender = ({ item }) => {
   const dispatch = useDispatch();
 
+  const handleClusterAction = useCallback(() => {
+    if (item?.status === CLUSTER_STATUS.DISCONNECTED) {
+      dispatch(
+        AuthenticationActions.setClusterLogin({
+          label: item.name,
+          value: item.id,
+        })
+      );
+    } else if (item?.status === CLUSTER_STATUS.CONNECTED) {
+      dispatch(GridActions.fetchGrid({ module: 'clusters' }));
+      dispatch(
+        GridActions.fetchGridSuccess({ module: 'namespaces', data: {} })
+      );
+      dispatch(DashboardActions.fetchDashboardSuccess({ data: {} }));
+      const clusterItem = localStorage.getItem('selected_cluster');
+      const clustersToken = JSON.parse(localStorage.getItem(CLUSTERS_TOKEN));
+
+      if (clusterItem) {
+        const cluster = JSON.parse(clusterItem);
+        if (cluster.value === item.id) {
+          localStorage.removeItem('selected_cluster');
+          dispatch(
+            NamespacesActions.setSelectedCluster({
+              label: '',
+              value: '',
+            })
+          );
+        }
+      }
+      // Remove the matching cluster from clustersToken
+      const updatedClustersToken = clustersToken.filter(
+        token => token.id !== item.id
+      );
+      localStorage.setItem(
+        CLUSTERS_TOKEN,
+        JSON.stringify(updatedClustersToken)
+      );
+      toast.success('Cluster Disconnected Successfully');
+    }
+  }, [dispatch, item]);
+
   return (
     <>
-      <EnableClusterText
-        onClick={() => {
-          if (item?.status === CLUSTER_STATUS.DISCONNECTED) {
-            dispatch(
-              AuthenticationActions.setClusterLogin({
-                label: item.name,
-                value: item.id,
-              })
-            );
+      <EnableClusterText onClick={handleClusterAction}>
+        <IconButton
+          title={
+            item?.status === CLUSTER_STATUS.DISCONNECTED
+              ? 'Login to Cluster'
+              : 'Logout from Cluster'
           }
-        }}
-      >
-        <IconButton disabled={item.status !== CLUSTER_STATUS.DISCONNECTED}>
-          {' '}
-          <LoginIcon />
+        >
+          {item?.status === CLUSTER_STATUS.DISCONNECTED ? (
+            <LoginIcon />
+          ) : (
+            <LogoutIcon />
+          )}
         </IconButton>
       </EnableClusterText>
       {item?.status === CLUSTER_STATUS.DISCONNECTED && (
@@ -55,5 +100,12 @@ export const EnableClusterRender = ({ item }) => {
 };
 
 EnableClusterRender.propTypes = {
-  item: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    status: PropTypes.oneOf([
+      CLUSTER_STATUS.DISCONNECTED,
+      CLUSTER_STATUS.CONNECTED,
+    ]).isRequired,
+  }).isRequired,
 };
