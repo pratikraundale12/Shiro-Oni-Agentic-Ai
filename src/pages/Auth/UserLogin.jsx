ff2a0542f6b250d82baf692ab56bf9c273a4e356
SourabH-0156 <sourabh.tamrakar@ksolves.com>
2024-12-02 18:35:45 +0530

    removed cluster field from user login

--- src/pages/Auth/UserLogin.jsx
+++ src/pages/Auth/UserLogin.jsx
@@ -8,13 +8,13 @@ import * as yup from 'yup';
 import { toast } from 'react-toastify';
 import { PasswordTextMessage, getRightIcon } from '.';
 import {
-  ClusterIcon,
+  // ClusterIcon,
   // GoogleIcon,
   LessArrowIcon,
   // MicroSoftIcon,
   UserIcon,
 } from '../../assets';
-import { ClusterSelect, Layout } from '../../components';
+import { Layout } from '../../components';
 import {
   FORGOT_PASSWORD,
   // GOOGLE,
@@ -101,7 +101,6 @@ const ForgetLinkContainer = styled.div`
 // `;
 
 const loginSchema = yup.object().shape({
-  cluster_id: yup.string().required('Cluster is required'),
   username: yup.string().required('Username is required'),
   password: yup.string().required('Password is required'),
 });
@@ -118,7 +117,6 @@ export const UserLogin = () => {
     watch,
     register,
     handleSubmit,
-    control,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(loginSchema),
@@ -134,18 +132,6 @@ export const UserLogin = () => {
       <SubTitle>{LOGIN_TO_YOUR_ACCOUNT}</SubTitle>
       <Form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <ClusterSelect
-            name="cluster_id"
-            control={control}
-            placeholder="Select a Cluster"
-            title="Select Cluster"
-            backgroundColor={theme.colors.white}
-            size="lg"
-            icon={<ClusterIcon />}
-            label="Select Cluster"
-            errors={errors}
-            required
-          />
           <InputField
             name="username"
             type="text"
