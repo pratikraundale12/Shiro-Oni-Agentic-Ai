ef7bfed2b58b7e5cb2de8d95c0cc83078da10694
SourabH-0156 <sourabh.tamrakar@ksolves.com>
2024-12-09 01:37:23 +0530

    setting payload

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -600,7 +600,28 @@ const Summary = () => {
   };
 
   const handleUpgradeByRegistry = () => {
-    dispatch(NamespacesActions.upgradeCluster('hello'));
+    const updatedData = paramterDeployArray.map(item => ({
+      parameterName: item.name,
+      parameters: item.parameters,
+    }));
+    console.log(registryDetailsData);
+    const payload = {
+      version: versionSelected,
+      flowId: checkDestCluster?.flowId,
+      bucketId: checkDestCluster?.bucketId,
+      registryId: checkDestCluster?.registryId,
+      position: {
+        x: XcordUpdated || checkDestCluster.position.x,
+        y: YcordUpdated || checkDestCluster.position.y,
+      },
+      variablesData: isEmpty(variblesReduxData)
+        ? registryDetailsData?.variablesData
+        : variblesReduxData,
+      parameterData: updatedData,
+      controllerServiceData: registryDetailsData?.controllerServicesData,
+      keep_existing_paramter_contexts: formDataRegistry?.keepParameters,
+    };
+    dispatch(NamespacesActions.upgradeCluster(payload));
   };
   const loadingregistry = useSelector(state =>
     LoadingSelectors.getLoading(state, 'deployNamespaceByRegistryFlow')
