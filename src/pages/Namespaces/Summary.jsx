c428d40434b03e58b4877eeabde4ca483ddfa162
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-12-09 14:36:56 +0530

    selcted cluster i summary upgrade page

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -701,9 +701,7 @@ const Summary = () => {
                       {KDFM.SELECTED_CLUSTER}
                     </SummaryDetailsHFourTag>
                     <SummaryDetailsPtag className="mb-0">
-                      {!deployByRegistryFlow
-                        ? currentSelectedCluster?.label
-                        : selectedDestCluster?.label}
+                      {currentSelectedCluster?.label}
                     </SummaryDetailsPtag>
                   </div>
                 </UseColXl>
