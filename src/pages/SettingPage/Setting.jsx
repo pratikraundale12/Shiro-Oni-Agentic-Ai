32d6d41129f60b55d24d49ca35407ce1a4244d23
sanjeet-ksolves <sanjeet@ksolves.com>
2024-12-09 20:12:37 +0530

    Update UI of setting page

--- src/pages/SettingPage/Setting.jsx
+++ src/pages/SettingPage/Setting.jsx
@@ -53,6 +53,17 @@ const LabelSelect = styled.div`
   line-height: 16px;
   color: ${props => props.theme.colors.darker};
 `;
+const HeadingContent = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 16px;
+  color: ${props => props.theme.colors.darker};
+`;
+const HeadingContentHr = styled.div`
+  margin-left: -24px;
+  margin-right: -24px;
+  border-bottom: 1px solid #ccc;
+`;
 
 const EmphasisText = styled.em`
   font-style: italic;
@@ -231,9 +242,12 @@ export const Setting = () => {
         }}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="d-flex justify-content-end me-4"></div>
+        <div className="d-flex justify-content-start me-4">
+          <HeadingContent>{KDFM.APP}</HeadingContent>
+        </div>
+        <HeadingContentHr className="mt-3 mb-4" />
         <InputFields className="row">
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6">
+          <div className="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6">
             <SelectField
               label="Refresh"
               name="refresh"
@@ -268,10 +282,20 @@ export const Setting = () => {
               errors={errors}
             />
           </div>
+          <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-8">
+            <InputField
+              name="title"
+              register={register}
+              icon={<QRIcons />}
+              label={KDFM.META_TITLE}
+              placeholder={KDFM.ENTER_META_TITLE}
+              errors={errors}
+            />
+          </div>
         </InputFields>
 
         <InputFields className="row">
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
             <UploadField
               name="logo"
               label="Logo"
@@ -285,7 +309,7 @@ export const Setting = () => {
               setValue={setValue}
             />
           </div>
-          <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
             <UploadField
               name="favicon"
               label="Favicon"
@@ -299,17 +323,10 @@ export const Setting = () => {
             />
           </div>
         </InputFields>
-        <div className="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-8">
-          <InputField
-            name="title"
-            register={register}
-            icon={<QRIcons />}
-            label={KDFM.META_TITLE}
-            placeholder={KDFM.ENTER_META_TITLE}
-            errors={errors}
-          />
+        <div className="d-flex justify-content-start me-4">
+          <HeadingContent>{KDFM.LDAP}</HeadingContent>
         </div>
-
+        <HeadingContentHr className="mt-3 mb-4" />
         <InputFields className="row">
           <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6 mb-4">
             <SelectField
@@ -343,7 +360,10 @@ export const Setting = () => {
             />
           </div>
         </InputFields>
-
+        <div className="d-flex justify-content-start me-4">
+          <HeadingContent>{KDFM.SCHEDULE_DIPLOYMENT}</HeadingContent>
+        </div>
+        <HeadingContentHr className="mt-3 mb-4" />
         <div className="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-8 mb-4">
           <SelectField
             // isMulti
@@ -383,9 +403,10 @@ export const Setting = () => {
             />
           </div>
         </InputFields>
-        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6 form-ele mt-1 mb-2">
-          <LabelSelect className="mb-3">Nifi Service Account Scope</LabelSelect>
+        <div className="d-flex justify-content-start me-4">
+          <HeadingContent>{KDFM.SERVICE_ACCOUNT}</HeadingContent>
         </div>
+        <HeadingContentHr className="mt-3 mb-4" />
         <div className="-flex justify-content-end me-4">
           <InputFields className="row">
             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-6">
