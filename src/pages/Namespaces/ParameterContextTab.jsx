61b35cdb1c6e2e53a7a674c3dd2caa68f6819f37
Shubhangiksolves <shubhangi.sharma@ksolves.com>
2024-12-10 18:56:17 +0530

    NMS-665 updated payload

--- src/pages/Namespaces/ParameterContextTab.jsx
+++ src/pages/Namespaces/ParameterContextTab.jsx
@@ -146,6 +146,7 @@ const ParameterContextTab = ({
             return null;
           })
           .filter(Boolean);
+
         if (isGroupUpdated) {
           const existingGroupIndex = updatedPayload[key].findIndex(
             g => g.name === group.name
@@ -154,11 +155,26 @@ const ParameterContextTab = ({
           if (existingGroupIndex > -1) {
             updatedPayload[key][existingGroupIndex] = {
               ...updatedPayload[key][existingGroupIndex],
-              parameters: [
-                ...updatedPayload[key][existingGroupIndex].parameters,
-                ...updatedParameters,
-              ],
+              parameters: updatedPayload[key][
+                existingGroupIndex
+              ].parameters.map(param => {
+                const updatedParam = updatedParameters.find(
+                  p => p.name === param.name
+                );
+                return updatedParam || param;
+              }),
             };
+            updatedParameters.forEach(updatedParam => {
+              if (
+                !updatedPayload[key][existingGroupIndex].parameters.some(
+                  p => p.name === updatedParam.name
+                )
+              ) {
+                updatedPayload[key][existingGroupIndex].parameters.push(
+                  updatedParam
+                );
+              }
+            });
           } else {
             updatedPayload[key].push({
               name: group.name,
@@ -169,7 +185,16 @@ const ParameterContextTab = ({
 
         return {
           ...group,
-          parameters: isGroupUpdated ? updatedParameters : group.parameters,
+          parameters: group.parameters.map(param => {
+            return param.name === data.name
+              ? {
+                  ...param,
+                  value: data.value,
+                  check: data.check,
+                  description: data.description,
+                }
+              : param;
+          }),
         };
       });
     });
@@ -185,15 +210,14 @@ const ParameterContextTab = ({
             return {
               ...group,
               parameters: group.parameters.map(parameter => {
-                if (parameter.name === data.name) {
-                  return {
-                    ...parameter,
-                    value: data.value,
-                    check: data.check,
-                    description: data.description,
-                  };
-                }
-                return parameter;
+                return parameter.name === data.name
+                  ? {
+                      ...parameter,
+                      value: data.value,
+                      check: data.check,
+                      description: data.description,
+                    }
+                  : parameter;
               }),
             };
           }
