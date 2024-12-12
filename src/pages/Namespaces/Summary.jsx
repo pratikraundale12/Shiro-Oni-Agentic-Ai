1baf02953576952dda91327fc6543d4f5f6e8b8e
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-12-12 12:54:58 +0530

    status change in upgrade click

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -408,6 +408,7 @@ const Summary = () => {
     text: '',
     forPopup: false,
   });
+  const [flowControlState, setFlowControlState] = useState(null);
   const summaryLoadingStateRedux = useSelector(
     NamespacesSelectors.getNamespaceSummaryLoadingState
   );
@@ -527,9 +528,7 @@ const Summary = () => {
 
   const handleConfirmUpdateStatus = () => {
     if (!deployByRegistryFlow) {
-      dispatch(
-        NamespacesActions.updateNamespaceStatusRegistry(confirmDialogue?.action)
-      );
+      setFlowControlState(confirmDialogue.action);
       if (confirmDialogue.forPopup) {
       } else {
         setActiveButton(confirmDialogue.action);
@@ -618,6 +617,9 @@ const Summary = () => {
       parameterName: item.name,
       parameters: item.parameters,
     }));
+    // dispatch(
+    //   NamespacesActions.updateNamespaceStatusRegistry(confirmDialogue?.action)
+    // );
     const payload = {
       version: versionSelected.version,
       namespaceId: checkDestCluster?.id,
@@ -633,6 +635,11 @@ const Summary = () => {
     }
     if (!isEmpty(controllerServiceReduxData)) {
       payload.payload.controllerServiceData = controllerServiceReduxData;
+    }
+    if (flowControlState) {
+      dispatch(
+        NamespacesActions.updateNamespaceStatusRegistry(flowControlState)
+      );
     }
     dispatch(NamespacesActions.upgradeCluster(payload));
     // toast.info(NamespacesSelectors.getUpdatedNamespaceResponse.message);
