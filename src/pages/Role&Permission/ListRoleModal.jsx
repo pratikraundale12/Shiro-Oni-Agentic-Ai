69084b8c63fd30689e4ee15de9d69a24d708fb78
naman-shukla-ksolves <naman.shukla@ksolves.com>
2024-12-17 15:09:41 +0530

    add serach functionality and some changes

--- src/pages/Role&Permission/ListRoleModal.jsx
+++ src/pages/Role&Permission/ListRoleModal.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Modal } from '../../shared';
-import { IconButton, Table, TextRender } from '../../components';
-import { DeleteSmallIcon, DisabledUserIcon, PencilIcon } from '../../assets';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import {
+  DisabledUserIcon,
+  NewLockIcon,
+  PencilIcon,
+  SmallSearchIcon,
+} from '../../assets';
+import { IconButton, Table, TextRender } from '../../components';
+import { Modal } from '../../shared';
+import {
+  AuthenticationSelectors,
   RolesActions,
   RolesSelectors,
-  AuthenticationSelectors,
 } from '../../store';
+import { theme } from '../../styles';
 
 const ModalBody = styled.div`
   position: relative;
@@ -18,6 +24,34 @@ const ModalBody = styled.div`
     th {
       background-color: #dde4f0 !important;
     }
+  }
+`;
+const SearchContainer = styled.div`
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 16px;
+    transform: translateY(-50%);
+  }
+`;
+
+const Search = styled.input`
+  width: 100%;
+  border-radius: 2px;
+  padding: 12px 12px 12px 40px;
+  font-size: 16px;
+  margin: 14px 0;
+  font-family: ${props => props.theme.fontRedHat};
+  border: 1px solid ${props => props.theme.colors.border};
+  background-color: ${props => props.theme.colors.lightGrey};
+
+  &:focus-visible {
+    outline: none;
+  }
+  @media screen and (max-width: 1400px) {
+    font-size: 14px !important;
   }
 `;
 const ListRoleModal = () => {
@@ -33,6 +67,11 @@ const ListRoleModal = () => {
     closePopup();
   };
   const userPermissions = useSelector(AuthenticationSelectors.getPermissions);
+  const [search, setSearch] = useState('');
+
+  const filteredRoles = roles?.filter(role =>
+    role.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   const COLUMNS = [
     {
@@ -40,13 +79,23 @@ const ListRoleModal = () => {
       renderCell: item => (
         <TextRender key={item?.name} text={item?.name} capitalizeText={true} />
       ),
-      width: '80%',
+      width: '40%',
+    },
+    {
+      label: 'Status',
+      renderCell: item => (
+        <TextRender
+          text={item?.is_active ? 'Active' : 'Inactive'}
+          capitalizeText={true}
+        />
+      ),
+      width: '40%',
     },
     {
       label: 'Actions',
       width: '20%',
       renderCell: item => (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', justifyContent: 'start', gap: 4 }}>
           {userPermissions.includes('edit_permission') && (
             <IconButton
               onClick={() => {
@@ -73,7 +122,7 @@ const ListRoleModal = () => {
                     dispatch(RolesActions.setIsRoleListModalOpen(false));
                   }}
                 >
-                  <DeleteSmallIcon color="black" />
+                  <NewLockIcon />
                 </IconButton>
               )}
             </>
@@ -100,10 +149,29 @@ const ListRoleModal = () => {
               onSubmit: handleOpenAddModal,
             }
           : { hideFooter: true })}
+        contentStyles={{ minWidth: '40%' }}
       >
+        <SearchContainer>
+          <SmallSearchIcon
+            width={18}
+            height={18}
+            color={theme.colors.darkGrey1}
+          />
+          <Search
+            type="search"
+            value={search}
+            placeholder="Search DFM access"
+            onChange={e => {
+              const value = e.target.value;
+              if (value.length <= 100) {
+                setSearch(value);
+              }
+            }}
+          />
+        </SearchContainer>
         <ModalBody className="modal-body">
           <Table
-            data={roles || []}
+            data={filteredRoles || []}
             columns={COLUMNS}
             className={'parameter-context-table'}
           />
