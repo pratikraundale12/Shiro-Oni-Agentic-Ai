9f9cd6bb53c78f52449cef1139b3fb0d8d0ffd07
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2024-12-24 17:33:27 +0530

    back time selection manually from the timestamp removed

--- src/pages/Namespaces/ScheduleDetailsPage.jsx
+++ src/pages/Namespaces/ScheduleDetailsPage.jsx
@@ -158,9 +158,19 @@ const ScheduleDeploymentTab = ({
   );
   const {
     control,
+    watch,
     formState: { errors },
     reset,
   } = useForm({});
+
+  const watchStatus = watch('scheduled_time');
+  const selectedStamp = new Date(watchStatus);
+  const currentDate = new Date();
+  if (selectedStamp < currentDate) {
+    reset({
+      scheduled_time: currentDate,
+    });
+  }
 
   useEffect(() => {
     if (timeDeployScheduleDeployment) {
