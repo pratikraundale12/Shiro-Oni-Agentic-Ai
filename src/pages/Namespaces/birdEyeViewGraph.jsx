9f0903c01d2edf1d7bea00d7f90ca91c5299c449
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-01-08 17:10:31 +0530

    code refactor

--- src/pages/Namespaces/birdEyeViewGraph.jsx
+++ src/pages/Namespaces/birdEyeViewGraph.jsx
@@ -2,7 +2,7 @@
 
 import * as d3 from 'd3';
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { NamespacesActions } from '../../store';
 import {
