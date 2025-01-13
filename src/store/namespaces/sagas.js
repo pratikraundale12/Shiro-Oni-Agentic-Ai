1022f5d050c2bb01074518212f77956f362b9e9b
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-01-13 11:22:13 +0530

    backend error from nifi display in frontend

--- src/store/namespaces/sagas.js
+++ src/store/namespaces/sagas.js
@@ -1247,7 +1247,9 @@ export function* upgradeCluster(api, { payload }) {
   }
 
   if (!response.ok) {
-    toast.error(response.message || KDFM.SOMETHING_WENT_WRONG);
+    toast.error(response?.data?.message || KDFM.SOMETHING_WENT_WRONG, {
+      autoClose: 5000,
+    });
   }
 }
 
