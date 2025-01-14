81c2694491305320409ec941c92f30d2c8d9bbf3
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-01-14 21:54:20 +0530

    css fixes

--- src/shared/FormInputs/components/DateRangePicker/index.jsx
+++ src/shared/FormInputs/components/DateRangePicker/index.jsx
@@ -5,13 +5,19 @@ import { DateRangePicker } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 
 const ContainerRangePicker = styled.div`
-  // margin: 20px;
   div > div {
     border: 1px solid #b5b5bd !important;
     color: black;
+    background-color: #f5f7fa;
   }
   div > div > input {
-    color: black;
+    color: black !important;
+    background-color: #f5f7fa;
+  }
+  div > div > input::placeholder {
+    color: #4c5055 !important;
+    opacity: 1; /* Ensures the color is not faded */
+    font-family: 'Red Hat Display', sans-serif;
   }
 `;
 const DateRangePickerInput = ({ value, handleChange, customRanges }) => {
