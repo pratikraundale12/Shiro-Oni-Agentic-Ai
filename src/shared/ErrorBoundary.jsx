a647f3a7af5b5c3f9f76486f7f7d0bcf54bec229
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-01-22 21:43:03 +0530

    remove dashboard button

--- src/shared/ErrorBoundary.jsx
+++ src/shared/ErrorBoundary.jsx
@@ -53,7 +53,7 @@ class ErrorBoundary extends React.Component {
 
   handleNavigation = url => {
     if (url === window.location.href) {
-      history.back(-1);
+      history.back();
     }
 
     this.setState({ hasError: false, errorMessage: '' }, () => {
