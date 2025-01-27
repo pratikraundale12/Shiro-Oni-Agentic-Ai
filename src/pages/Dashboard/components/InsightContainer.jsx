56f063b24d6da146b80e1c624a3ebed3cb8f519d
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-01-27 18:38:51 +0530

    without cluter login not navigate to nifi

--- src/pages/Dashboard/components/InsightContainer.jsx
+++ src/pages/Dashboard/components/InsightContainer.jsx
@@ -63,6 +63,7 @@ export const InsightContainer = ({
   selectedNamespace,
 }) => {
   const dashboardData = useSelector(DashboardSelectors.getDashboardData);
+  const getActiveCluster = localStorage.getItem('selected_cluster');
   const handleClick = () => {
     if (selectedNamespace?.value) {
       const updatedUrl = dashboardData?.namespaces?.nifiUrl.endsWith('/nifi')
@@ -81,8 +82,9 @@ export const InsightContainer = ({
     <Container className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-6">
       <InnerContainer
         backgroundCss={backgroundCss}
-        onClick={handleClick}
+        onClick={getActiveCluster?.label ? handleClick : undefined}
         data-tooltip-id={`tooltip-${text}`}
+        style={{ cursor: getActiveCluster?.label ? 'pointer' : 'not-allowed' }}
       >
         <IconContainer>
           <Icon />
@@ -92,12 +94,14 @@ export const InsightContainer = ({
         </CountDisplay>
         <InsightText>{text}</InsightText>
       </InnerContainer>
-      <ReactTooltip
-        id={`tooltip-${text}`}
-        place="top"
-        effect="solid"
-        content={`Click on ${text} to view details`}
-      />
+      {getActiveCluster?.label && (
+        <ReactTooltip
+          id={`tooltip-${text}`}
+          place="top"
+          effect="solid"
+          content={`Click on ${text} to view details`}
+        />
+      )}
     </Container>
   );
 };
