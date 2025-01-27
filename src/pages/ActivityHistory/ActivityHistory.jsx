e9c7ce3cc6d1d1387f4f959f2ad4381dde971911
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-01-27 14:00:35 +0530

    consition ellipses for table

--- src/pages/ActivityHistory/ActivityHistory.jsx
+++ src/pages/ActivityHistory/ActivityHistory.jsx
@@ -204,7 +204,12 @@ export const ActvityHistory = () => {
         </>
       ),
       width: '10%',
-      renderCell: item => <TextRender text={item.created_by_name || KDFM.NA} />,
+      renderCell: item => (
+        <TextRender
+          text={item.created_by_name || KDFM.NA}
+          withEllipses={true}
+        />
+      ),
     },
   ];
 
