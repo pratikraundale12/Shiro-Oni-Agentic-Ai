31e388790d63973aafffa19f09b15ad6acb78f7e
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-02-05 17:47:05 +0530

    comment fix

--- src/pages/Namespaces/ListNamespaces.jsx
+++ src/pages/Namespaces/ListNamespaces.jsx
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
@@ -124,8 +125,8 @@ export const ListNamespaces = () => {
           <>
             <li>Name : {item?.name}</li>
             {<li>ID : {item?.id}</li>}
-            {search?.length > 0 && item?.parent && (
-              <li>Parent : {item?.parent}</li>
+            {!isEmpty(search) && item?.parent && (
+              <li>Parent : {item.parent}</li>
             )}
           </>
         )}
