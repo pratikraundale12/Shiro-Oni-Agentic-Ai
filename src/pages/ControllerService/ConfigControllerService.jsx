50ceabf3c2941515091c8f22391245949be9ee29
vaibhav-ksolves1 <vaibhav.dhakad@ksolves.com>
2025-02-10 13:37:09 +0530

    some improvement in seed data

--- src/pages/ControllerService/ConfigControllerService.jsx
+++ src/pages/ControllerService/ConfigControllerService.jsx
@@ -225,11 +225,12 @@ export const ConfigControllerService = ({
           )}
           <div
             style={{ cursor: 'pointer' }}
-            onClick={() =>
+            onClick={() => {
+              console.log(item);
               !item?.isSelective
                 ? handleAddValueModal(item)
-                : handleOpenPropertyDropDownModal(item)
-            }
+                : handleOpenPropertyDropDownModal(item);
+            }}
           >
             {' '}
             <PencilIcon height="28" />
