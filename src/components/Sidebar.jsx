b43fea4d0ecf4c2ce9bc312e29863daa13f3ac85
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-02-13 18:25:59 +0530

    version updated to 2.1.7

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -323,7 +323,7 @@ export const Sidebar = ({
 
       <KDFMVersion>
         {/* FIX_ME: Later will come from API */}
-        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.5`}</span>
+        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.7`}</span>
       </KDFMVersion>
     </Container>
   );
