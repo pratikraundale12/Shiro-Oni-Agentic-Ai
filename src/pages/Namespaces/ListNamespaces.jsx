e20d33edd4ff77095bb3ff2722543b2ffa4590b0
Shubhangiksolves <shubhangi.sharma@ksolves.com>
2025-02-20 17:08:47 +0530

    Reverted DFM-1086 changes regarding pagination

--- src/pages/Namespaces/ListNamespaces.jsx
+++ src/pages/Namespaces/ListNamespaces.jsx
@@ -465,6 +465,7 @@ export const ListNamespaces = () => {
         state={state}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
+        itemsPerPage={KDFM.ITEMS_PER_PAGE}
       />
     </>
   );
