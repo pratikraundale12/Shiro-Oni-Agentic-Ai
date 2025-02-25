5b1fc28b730b5e4a486e81928dde33f8f4544f2e
Rahul Bhardwaj <rahul.bhardwaj@ksolves.com>
2025-02-25 17:47:34 +0530

    created build ready code for frontend

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ import { USER_CONSTANTS } from './user.constant';
 export * from './login';
 
 // generalconstants
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const API_URL = window.env?.REACT_APP_API_URL || process.env.REACT_APP_API_URL;
 export const ACCESS_TOKEN = 'access_token';
 export const CLUSTERS_TOKEN = 'clusters';
 
