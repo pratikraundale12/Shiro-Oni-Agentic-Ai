73450812580a442b7ee4ac3b990df14d53185f27
Rahul Bhardwaj <rahul.bhardwaj@ksolves.com>
2025-03-13 09:37:40 +0530

    fixed a check to display production license

--- src/constants/index.js
+++ src/constants/index.js
@@ -136,12 +136,11 @@ export const SSO_LOGIN_TYPE = [{ label: 'Azure', value: 'azure' }];
 
 export const LICENSE_TYPE = {
   TRIAL: 'trial',
-  PURCHASED: 'purchased',
+  PURCHASED: 'production',
 };
 
-export const LICENSE_DATE_ISO_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSZ';
-export const LICENSE_EXPIRE_PROMPT_DAYS =
-  process.env.REACT_APP_LICENSE_EXPIRE_TIMESPAN_IN_DAYS || 30;
+export const LICENSE_DATE_ISO_FORMAT = 'MMMM D, YYYY [at] h:mm:ss A';
+export const LICENSE_EXPIRE_PROMPT_DAYS = 30;
 
 export const KDFM = {
   // Generic constants
@@ -247,7 +246,7 @@ export const KDFM = {
   TRIAL_EXPIRED_PROMPT: arg =>
     `Trial: Your trial license will expire on ${arg}.`,
   PURCHASED_EXPIRED_PROMPT: arg =>
-    `Licensed: Your purchased license will expire on ${arg}. Please renew it to continue using the platform.`,
+    `Licensed: Your license will expire on ${arg}. Please renew it to continue using the platform.`,
 
   // module specific constants
   ...CLUSTER_CONSTANTS,
