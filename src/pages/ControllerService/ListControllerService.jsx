af974e8791c346757ad734a71a7e5fed91e327c9
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-03-19 11:16:02 +0530

    dfm 1211 ticket resolved

--- src/pages/ControllerService/ListControllerService.jsx
+++ src/pages/ControllerService/ListControllerService.jsx
@@ -168,11 +168,13 @@ export const ListControllerService = () => {
   const [isResetNotRequired, setIsResetNotRequired] = useState(false);
   const filteredModulesData = useMemo(
     () =>
-      listData?.filter(
-        module =>
-          module?.name?.toLowerCase().includes(search.toLowerCase()) ||
-          module?.type?.toLowerCase().includes(search.toLowerCase())
-      ),
+      !isEmpty(listData)
+        ? listData?.filter(
+            module =>
+              module?.name?.toLowerCase().includes(search.toLowerCase()) ||
+              module?.type?.toLowerCase().includes(search.toLowerCase())
+          )
+        : [],
     [listData, search]
   );
   const [isUserCanWrite, setIsUserCanWrite] = useState(listData?.[0]?.canWrite);
@@ -504,10 +506,12 @@ export const ListControllerService = () => {
   };
 
   const handleRefresh = () => {
-    setSearch('');
-    setSearchErrorMsg({});
-    setSearchText('');
-    dispatch(NamespacesActions.getControllerServiceList());
+    if (selectedCluster?.value && !isEmpty(selectedCluster?.value)) {
+      setSearch('');
+      setSearchErrorMsg({});
+      setSearchText('');
+      dispatch(NamespacesActions.getControllerServiceList());
+    }
   };
 
   const statusLoading = useSelector(state =>
