716b22a9c0756627fbd70adb7d5c6228aef9df16
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-04-02 14:57:29 +0530

    changes

--- src/pages/SettingPage/SettingTab.jsx
+++ src/pages/SettingPage/SettingTab.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import {
+  AppIcon,
+  DeploymentScheduleIcon,
+  EmailConfigIcon,
+  LDAPIcon,
+  SSOLoginIcon,
+  ServiceAccountIcon,
+} from '../../assets';
+import { AppSettings } from './AppSettings';
+import { DeploymentScheduleSettings } from './DeploymentScheduleSettings';
+import { EmailConfigurationSettings } from './EmailConfigurationSettings';
 import FlowValidation from './FlowValidation';
+import { LDAPSettings } from './LDAPSettings';
+import { SSOLoginSettings } from './SSOLoginSettings';
+import { ServiceAccountSettings } from './ServiceAccountSettings';
 import { Setting } from './Setting';
 
 const GreyBoxNamespace = styled.div`
-  background-color: #f5f7fa;
+  background-color: #ffffff;
   padding: 22px 19px;
   border-radius: 20px;
 `;
@@ -13,6 +27,10 @@ const TabWrapper = styled.div`
   margin-bottom: 1rem;
   align-items: flex-start;
   border-bottom: 1px solid rgba(221, 228, 240, 1);
+  flex-wrap: nowrap; /* Prevents tabs from wrapping */
+  align-items: center;
+  border-bottom: 1px solid rgba(221, 228, 240, 1);
+  min-width: max-content; /* Ensures it doesn't shrink below content width */
 `;
 
 const Tab = styled.div`
@@ -27,23 +45,61 @@ const Tab = styled.div`
     props.active ? 'rgba(255, 122, 0, 1)' : 'rgba(68, 68, 69, 1)'};
   border-color: ${props =>
     props.active ? 'rgba(255, 122, 0, 1)' : 'transparent'};
+  svg {
+    stroke: ${props =>
+      props.active ? 'rgba(255, 122, 0, 1)' : 'rgba(68, 68, 69, 1)'};
+    transition: stroke 0.3s;
+  }
   &:hover {
     color: rgba(255, 122, 0, 1);
+
+    svg {
+      stroke: rgba(255, 122, 0, 1);
+    }
   }
 `;
+
+const TabsContainer = styled.div`
+  width: 100%;
+  overflow-x: auto; /* Enables horizontal scrolling */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for Firefox */
+  scrollbar-width: none;
+`;
+
 const TabContent = styled.div`
   width: 100%;
   padding: 1rem;
   border-radius: 0.25rem;
-  background-color: #f8f9fa;
+  background-color: #ffffff;
+`;
+
+const IconContent = styled.div`
+  display: inline;
+  margin-right: 6px;
 `;
 
 const SettingTab = () => {
-  const [activeTab, setActiveTab] = useState('Setting');
+  const [activeTab, setActiveTab] = useState('AppSettings');
   const renderContent = () => {
     switch (activeTab) {
       case 'Setting':
         return <Setting />;
+      case 'AppSettings':
+        return <AppSettings />;
+      case 'LDAPSettings':
+        return <LDAPSettings />;
+      case 'DeploymentScheduleSettings':
+        return <DeploymentScheduleSettings />;
+      case 'ServiceAccountSettings':
+        return <ServiceAccountSettings />;
+      case 'EmailConfigurationSettings':
+        return <EmailConfigurationSettings />;
+      case 'SSOLoginSettings':
+        return <SSOLoginSettings />;
       case 'Flow Validation':
         return <FlowValidation />;
     }
@@ -51,22 +107,81 @@ const SettingTab = () => {
   return (
     <div>
       <GreyBoxNamespace className="w-100  mb-3">
-        <TabWrapper className="nav">
-          <Tab
-            active={activeTab === 'Setting'}
-            onClick={() => setActiveTab('Setting')}
-            className="nav-item"
-          >
-            General Settings
-          </Tab>
-          <Tab
-            active={activeTab === 'Flow Validation'}
-            onClick={() => setActiveTab('Flow Validation')}
-            className="nav-item"
-          >
-            Flow Validation
-          </Tab>
-        </TabWrapper>
+        <TabsContainer>
+          <TabWrapper className="nav">
+            <Tab
+              active={activeTab === 'AppSettings'}
+              onClick={() => setActiveTab('AppSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <AppIcon />
+              </IconContent>
+              App
+            </Tab>
+            <Tab
+              active={activeTab === 'LDAPSettings'}
+              onClick={() => setActiveTab('LDAPSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <LDAPIcon />
+              </IconContent>
+              LDAP
+            </Tab>
+            <Tab
+              active={activeTab === 'DeploymentScheduleSettings'}
+              onClick={() => setActiveTab('DeploymentScheduleSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <DeploymentScheduleIcon />
+              </IconContent>
+              Deployment Schedule
+            </Tab>
+            <Tab
+              active={activeTab === 'ServiceAccountSettings'}
+              onClick={() => setActiveTab('ServiceAccountSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <ServiceAccountIcon />
+              </IconContent>
+              Service Account
+            </Tab>
+            <Tab
+              active={activeTab === 'EmailConfigurationSettings'}
+              onClick={() => setActiveTab('EmailConfigurationSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <EmailConfigIcon />
+              </IconContent>
+              Email Configuration
+            </Tab>
+            <Tab
+              active={activeTab === 'SSOLoginSettings'}
+              onClick={() => setActiveTab('SSOLoginSettings')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <SSOLoginIcon />
+              </IconContent>
+              SSO Login
+            </Tab>
+
+            <Tab
+              active={activeTab === 'Flow Validation'}
+              onClick={() => setActiveTab('Flow Validation')}
+              className="nav-item"
+            >
+              <IconContent className="nav-item">
+                <SSOLoginIcon />
+              </IconContent>
+              Flow Validation
+            </Tab>
+          </TabWrapper>
+        </TabsContainer>
         <TabContent>{renderContent()}</TabContent>
       </GreyBoxNamespace>
     </div>
