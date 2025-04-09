51456f1b15213e8ec129d47dea9c894c2162958b
ankitanjana23 <ankitanjana@gmail.com>
2025-04-10 03:39:47 +0530

    add log export features for setting tab

--- src/store/services2/setting.js
+++ src/store/services2/setting.js
@@ -1,8 +1,13 @@
 export const settingsAPI = api => {
   const fetchSettings = ({ params = {} }) => api.get('/settings', params);
   const createSettings = ({ payload = {} }) => api.patch('/settings', payload);
+  const downloadLogsZip = ({ payload = {} }) =>
+    api.post('/logs/download-zip', payload, {
+      responseType: 'blob', //saga response change here
+    });
   return {
     createSettings,
     fetchSettings,
+    downloadLogsZip,
   };
 };
