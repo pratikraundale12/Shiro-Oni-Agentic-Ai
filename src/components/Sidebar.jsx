a7561a4e683193c684cc6aed11e969f40c04d7a0
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-04-18 14:00:15 +0530

    version change to 2.1.14

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -323,7 +323,7 @@ export const Sidebar = ({
 
       <KDFMVersion>
         {/* FIX_ME: Later will come from API */}
-        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.13`}</span>
+        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.14`}</span>
       </KDFMVersion>
     </Container>
   );
