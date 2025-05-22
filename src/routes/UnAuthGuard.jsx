837cafc97410c6d7f934ded0386b4b6c8fb1be29
Rahul Bhardwaj <rahul.bhardwaj@ksolves.com>
2025-05-22 14:42:42 +0530

    changed version to 2.1.18

--- src/routes/UnAuthGuard.jsx
+++ src/routes/UnAuthGuard.jsx
@@ -159,7 +159,7 @@ const UnAuthGuard = () => {
         </List>
         <KDFMVersion>
           {/* FIX_ME: Later will come from API */}
-          <span className="version-content">Version 2.1.17</span>
+          <span className="version-content">Version 2.1.18</span>
         </KDFMVersion>
       </Container>
       <Content>
