9104925e05483d5a1bab8f138336db0bdc70c132
KSMayurSarkar <mayur.sarkar@ksolves.com>
2025-05-22 17:07:55 +0530

    Created charts for cluster summary statistics

--- src/pages/Clusters/components/ClusterStatusTab.jsx
+++ src/pages/Clusters/components/ClusterStatusTab.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,6 +11,7 @@ import { useParams } from 'react-router-dom';
 import { FullPageLoader } from '../../../components';
 import { isEmpty } from 'lodash';
 import { theme } from '../../../styles';
+import Chart from 'react-apexcharts';
 
 const RowConfig = styled.div`
   display: flex;
@@ -44,6 +46,7 @@ const SummaryDetailsHFourTag = styled.h4`
   text-align: left;
   color: #2d343f;
 `;
+
 const SummaryDetailsPtag = styled.h4`
   font-family: ${props => props.theme.fontRedHat};
   font-size: 14px;
@@ -62,6 +65,7 @@ const SummaryDetailsPtag = styled.h4`
     }
   }
 `;
+
 const DataWrapper = styled.div`
   width: 100%;
   height: 596px;
@@ -87,6 +91,77 @@ const TitleText = styled.div`
   letter-spacing: -0.5%;
   text-transform: capitalize;
 `;
+
+const ChartColumnWrapper = styled.div`
+  flex: 0 0 auto;
+  width: 50%;
+  
+  @media screen and (min-width: 1200px) {
+    width: 25%;
+  }
+  
+  padding-right: 1rem;
+  padding-left: 1rem;
+  margin-bottom: 1.5rem;
+`;
+
+const ChartCard = styled.div`
+  border: 1px solid #e0e0e0;
+  border-radius: 12px;
+  padding: 20px;
+  background-color: #fff;
+  height: 250px;
+`;
+
+const ChartTitle = styled.h6`
+  color: #666;
+  font-size: 18px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+const ChartContainer = styled.div`
+  height: 150px;
+  position: relative;
+`;
+
+const ChartLabelsContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  margin-top: 33px;
+`;
+
+const ChartLabelsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+`;
+
+const ChartUnit = styled.span`
+  font-size: 16px;
+  color: #666;
+`;
+
+const ChartSubtitle = styled.span`
+  font-size: 16px;
+  color: #666;
+`;
+
+const NodeCard = styled.div`
+  border: 1px solid #dde4f0;
+  border-radius: 16px;
+  background-color: #fff;
+  margin-bottom: 20px;
+`;
+
+const NodeCardContent = styled.div`
+  padding: 1rem;
+`;
+
 const ClusterStatusTab = () => {
   const dispatch = useDispatch();
   const { id: clusterId } = useParams();
@@ -108,6 +183,101 @@ const ClusterStatusTab = () => {
     return () => clearInterval(intervalId);
   }, [dispatch, clusterId]);
 
+  // Helper function to extract percentage value from strings like "24.77%"
+  const getPercentageValue = value => {
+    if (!value) return 0;
+    const numericValue = parseFloat(value.toString().replace('%', ''));
+    return isNaN(numericValue) ? 0 : numericValue;
+  };
+
+  const RadialChart = ({ title, value, unit, subtitle, percentage, showPercentageSign }) => {
+    const percentageValue = getPercentageValue(percentage);
+
+    const chartOptions = {
+      chart: {
+        type: 'radialBar',
+        height: 200,
+        sparkline: {
+          enabled: true,
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: 0,
+          endAngle: 360,
+          hollow: {
+            size: '55%',
+          },
+          track: {
+            background: '#f0f0f0',
+            strokeWidth: '100%',
+            margin: 5,
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              show: true,
+              fontSize: '20px',
+              fontWeight: 'bold',
+              color: '#333',
+              offsetY: -5,
+              formatter: function () {
+                let val =
+                  typeof value === 'string'
+                    ? value.replace(/[a-zA-Z]+/, '')
+                    : value;
+
+                const num = parseFloat(val);
+                if (isNaN(num)) return '00.00';
+
+                const parts = num.toFixed(2).split('.');
+                const paddedInteger = parts[0].padStart(2, '0');
+                return `${paddedInteger}.${parts[1]}${showPercentageSign ? '%' : ''}`;
+              },
+            },
+          },
+        },
+      },
+      colors: ['#F07A0D'],
+      stroke: {
+        lineCap: 'round',
+      },
+      labels: [title],
+    };
+
+    const series = [percentageValue || 0];
+
+    return (
+      <ChartColumnWrapper className="col-xl-3 col-6 mb-4">
+        <ChartCard>
+          <ChartTitle>
+            {title}
+          </ChartTitle>
+          <ChartContainer>
+            <Chart
+              options={chartOptions}
+              series={series}
+              type="radialBar"
+              height={200}
+            />
+            <ChartLabelsContainer>
+              <ChartLabelsWrapper>
+                <ChartUnit>{unit}</ChartUnit>
+                {subtitle && (
+                  <ChartSubtitle>
+                    ({subtitle})
+                  </ChartSubtitle>
+                )}
+              </ChartLabelsWrapper>
+            </ChartLabelsContainer>
+          </ChartContainer>
+        </ChartCard>
+      </ChartColumnWrapper>
+    );
+  };
+
   return (
     <DataWrapper className="w-100">
       <FullPageLoader loading={loading} />
@@ -115,97 +285,79 @@ const ClusterStatusTab = () => {
         {healthMetricData &&
           !isEmpty(healthMetricData) &&
           healthMetricData?.data?.map(ele => (
-            <div
+            <NodeCard
               key={ele?.name}
-              style={{ border: '1px solid #dde4f0', borderRadius: '16px' }}
               className="mb-2"
             >
-              <TitleText className="mt-4 ms-3">
-                {' '}
-                <span style={{ color: `${theme.colors.primary}` }}>
-                  Node{' '}
-                </span>: {ele?.name}
+              <TitleText className="mt-4 ms-3 mb-3">
+                <span style={{ color: `${theme.colors.primary}` }}>Node</span>:{' '}
+                {ele?.name}
               </TitleText>
-              <RowConfig className=" p-3">
-                <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                  <div>
-                    <SummaryDetailsHFourTag className="mb-2">
-                      Total Disk
-                    </SummaryDetailsHFourTag>
-                    <SummaryDetailsPtag className="mb-0">
-                      {ele?.data?.disk?.total}
-                    </SummaryDetailsPtag>
-                  </div>
-                </UseColXl>
-                <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                  <div>
-                    <SummaryDetailsHFourTag className="mb-2">
-                      Total Disk Utilisation
-                    </SummaryDetailsHFourTag>
-                    <SummaryDetailsPtag className="mb-0">
-                      {ele?.data?.disk?.used} &nbsp;
-                      {`(${ele?.data?.disk?.utilization})`}
-                    </SummaryDetailsPtag>
-                  </div>
-                </UseColXl>
-                <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                  <div>
-                    <SummaryDetailsHFourTag className="mb-2">
-                      CPU Utilisation
-                    </SummaryDetailsHFourTag>
-                    <SummaryDetailsPtag className="mb-0">
-                      {ele?.data?.cpu?.utilization}
-                    </SummaryDetailsPtag>
-                  </div>
-                </UseColXl>
-                <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                  <div>
-                    <SummaryDetailsHFourTag className="mb-2">
-                      Total Memory
-                    </SummaryDetailsHFourTag>
-                    <SummaryDetailsPtag className="mb-0">
-                      {ele?.data?.memory?.total}
-                    </SummaryDetailsPtag>
-                  </div>
-                </UseColXl>
-                <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                  <div>
-                    <SummaryDetailsHFourTag className="mb-2">
-                      Total Memory Utilisation
-                    </SummaryDetailsHFourTag>
-                    <SummaryDetailsPtag className="mb-0">
-                      {ele?.data?.memory?.used}
-                      &nbsp;
-                      {`(${ele?.data?.memory?.utilization})`}
-                    </SummaryDetailsPtag>
-                  </div>
-                </UseColXl>
-                {ele?.data?.nifi?.heap_used && (
-                  <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                    <div>
-                      <SummaryDetailsHFourTag className="mb-2">
-                        NiFi Heap Used
-                      </SummaryDetailsHFourTag>
-                      <SummaryDetailsPtag className="mb-0">
-                        {ele?.data?.nifi?.heap_used}
-                      </SummaryDetailsPtag>
-                    </div>
-                  </UseColXl>
-                )}
-                {ele?.data?.nifi?.heap_assigned && (
-                  <UseColXl className="col-xl-4 col-6 mb-4 pb-1">
-                    <div>
-                      <SummaryDetailsHFourTag className="mb-2">
-                        NiFi Heap Assigned
-                      </SummaryDetailsHFourTag>
-                      <SummaryDetailsPtag className="mb-0">
-                        {ele?.data?.nifi?.heap_assigned}
-                      </SummaryDetailsPtag>
-                    </div>
-                  </UseColXl>
-                )}
-              </RowConfig>
-            </div>
+
+              <NodeCardContent>
+                <RowConfig className="row">
+                  <RadialChart
+                    title="Total Disk"
+                    value={ele?.data?.disk?.total || '0'}
+                    unit="GB"
+                    percentage={0}
+                  />
+
+                  <RadialChart
+                    title="Total Disk Utilisation"
+                    value={ele?.data?.disk?.used || '0'}
+                    unit="GB"
+                    subtitle={ele?.data?.disk?.utilization}
+                    percentage={ele?.data?.disk?.utilization}
+                  />
+
+                  <RadialChart
+                    title="CPU Utilisation"
+                    value={
+                      getPercentageValue(ele?.data?.cpu?.utilization).toFixed(
+                        2
+                      ) + '%' || '0.00%'
+                    }
+                    unit=""
+                    percentage={ele?.data?.cpu?.utilization}
+                    showPercentageSign={true}
+                  />
+
+                  <RadialChart
+                    title="Total Memory"
+                    value={ele?.data?.memory?.total || '0'}
+                    unit="GB"
+                    percentage={0}
+                  />
+
+                  <RadialChart
+                    title="Total Memory Utilisation"
+                    value={ele?.data?.memory?.used || '0'}
+                    unit="GB"
+                    subtitle={ele?.data?.memory?.utilization}
+                    percentage={ele?.data?.memory?.utilization}
+                  />
+
+                  {ele?.data?.nifi?.heap_used && (
+                    <RadialChart
+                      title="NiFi Heap Used"
+                      value={ele?.data?.nifi?.heap_used || '0'}
+                      unit="MB"
+                      percentage={0}
+                    />
+                  )}
+
+                  {ele?.data?.nifi?.heap_assigned && (
+                    <RadialChart
+                      title="NiFi Heap Assigned"
+                      value={ele?.data?.nifi?.heap_assigned || '0'}
+                      unit="MB"
+                      percentage={0}
+                    />
+                  )}
+                </RowConfig>
+              </NodeCardContent>
+            </NodeCard>
           ))}
       </ScrollSetGrey>
     </DataWrapper>
