5d6f318b9f7b12457fb601fef53af1fb6293fba8
vaibhav-ksolves1 <vaibhav.dhakad@ksolves.com>
2025-06-18 14:05:34 +0530

    UPdated text for chedule start and stop

--- src/pages/Namespaces/ConfigDetailsPage.jsx
+++ src/pages/Namespaces/ConfigDetailsPage.jsx
@@ -117,7 +117,8 @@ const ConfigDetailsPage = () => {
   const selectedNameSpace = useSelector(
     NamespacesSelectors.getSelectedNamespace
   );
-
+  const scheduleFlowType = useSelector(NamespacesSelectors.getScheduleFlowType);
+  console.log('wwwwwwwwwwwww SCEHD', scheduleFlowType);
   const breadcrumbDataOnUpgrade = scheduleStartFlow
     ? [
         {
@@ -159,6 +160,10 @@ const ConfigDetailsPage = () => {
   let type = '';
   if (versionSelected?.version > singleNameSpace?.version) {
     type = 'upgrade';
+  } else if (scheduleFlowType === 'RUNNING') {
+    type = 'start';
+  } else if (scheduleFlowType === 'STOPPED') {
+    type = 'stop';
   } else {
     type = 'downgrade';
   }
