f251e1254472831eecc5f06327e1bcf89389749f
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-06-23 13:09:15 +0530

    version change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -652,7 +652,7 @@ export const Layout = ({ children }) => {
                     Terms Of Use
                   </RedirectionText>
                 </PolicyContainer>
-                <LabelSelect>Version 2.1.20</LabelSelect>
+                <LabelSelect>Version 2.1.21</LabelSelect>
               </RightSection>
             ) : (
               <RightSectionreset>
@@ -682,7 +682,7 @@ export const Layout = ({ children }) => {
                     Terms Of Use
                   </RedirectionText>
                 </PolicyContainer>
-                <LabelSelect>Version 2.1.20</LabelSelect>
+                <LabelSelect>Version 2.1.21</LabelSelect>
               </RightSectionreset>
             )}
           </RightWrapper>
