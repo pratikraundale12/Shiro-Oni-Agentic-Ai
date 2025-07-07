2a08ba64fea606b8d46b8c5373f685500e76970a
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-07-07 08:25:18 +0530

    changes

--- src/components/CustomGrid/Pagination.jsx
+++ src/components/CustomGrid/Pagination.jsx
@@ -1,15 +1,9 @@
-/* eslint-disable react/prop-types */
 import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { GreaterArrowIcon, LessArrowIcon } from '../../assets';
 import { theme } from '../../styles';
 import { PAGINATION_ITEM_OPTIONS } from '../../constants';
-import { Button } from '../../shared';
-import { ActivityHistoryActions } from '../../store/activityHistory';
-import { useDispatch, useSelector } from 'react-redux';
-import { FullPageLoader } from '../FullPageLoader';
-import { LoadingSelectors } from '../../store';
 
 const Container = styled.div`
   display: flex;
@@ -93,14 +87,8 @@ const Pagination = ({
   itemsPerPage,
   onItemsPerPageChange,
   setPageLoading,
-  selectEntity,
-  selectStatus,
-  selectEvent,
 }) => {
   const totalPage = Math.ceil(count / itemsPerPage);
-
-  // console.log(selectEntity, selectStatus, selectEvent);
-
   const getPageRange = () => {
     const start = (page - 1) * itemsPerPage + 1;
     const end = Math.min(count, page * itemsPerPage);
@@ -172,27 +160,8 @@ const Pagination = ({
     setPageLoading && setPageLoading(false);
   }, [page]);
 
-  const dispatch = useDispatch();
-
-  const handleExportReport = () => {
-    dispatch(
-      ActivityHistoryActions.fetchEmailReport({
-        queryParams: {
-          status: selectStatus.map(status => status.value).join(','),
-          event: selectEvent.map(event => event.value).join(','),
-          entity: selectEntity.map(entity => entity.value).join(','),
-        },
-      })
-    );
-  };
-
-  const loading = useSelector(state =>
-    LoadingSelectors.getLoading(state, 'fetchEmailReport')
-  );
-
   return (
     <Container>
-      <FullPageLoader loading={loading} />
       <div className="d-flex align-items-center gap-3">
         <span>{`${getPageRange()} of ${count} List`}</span>
         <Select
@@ -208,22 +177,6 @@ const Pagination = ({
         <span>Items per page</span>
       </div>
       <Flex>
-        {window.location.pathname === '/activity-history' && (
-          <div className="d-flex align-items-center gap-2">
-            <div>
-              <Button>Download</Button>
-            </div>
-            <div>
-              <Button
-                onClick={() => {
-                  handleExportReport();
-                }}
-              >
-                Export Report
-              </Button>
-            </div>
-          </div>
-        )}
         <StyledButton onClick={handlePrev} disabled={page === 1}>
           <GreaterArrowIcon color={theme.colors.white} />
         </StyledButton>
