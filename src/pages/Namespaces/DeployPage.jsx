7d0f843db776111aa5f543c3d786eb0539a37259
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-07-23 12:15:23 +0530

    Keep existing parameter context issue fix

--- src/pages/Namespaces/DeployPage.jsx
+++ src/pages/Namespaces/DeployPage.jsx
@@ -179,10 +179,11 @@ function DeployPage() {
   const [successTest, setSuccessTest] = useState(false);
   const [proceedWithDispatch, setProceedWithDispatch] = useState(false);
   const formData = useSelector(NamespacesSelectors.getDeployFormData);
-  const [keepParameter, setKeepParameter] = useState(true);
   const scheduleDeploymentFlow = useSelector(
     NamespacesSelectors.getScheduleByRegistry
   );
+  const keepParameters = useSelector(NamespacesSelectors.getKeepParameters);
+
   const tableRef = useRef(null);
 
   const handleScrollOnClick = () => {
@@ -427,7 +428,7 @@ function DeployPage() {
           bucketId: selectedValuebucketId,
           flow_name: selectedValueFlowId,
           selectedFlowName: flowname?.[0]?.label,
-          keepParameters: keepParameter,
+          keepParameters: keepParameters,
         })
       );
       dispatch(NamespacesActions.fetchRegistryFlowDetails(versionSelected));
@@ -442,7 +443,7 @@ function DeployPage() {
         bucketId: value.value,
         flow_name: '',
         selectedFlowName: '',
-        keepParameters: keepParameter,
+        keepParameters: keepParameters,
       })
     );
     dispatch(NamespacesActions.setVersionSelect(''));
@@ -467,6 +468,7 @@ function DeployPage() {
   const loadingfetchRegistryFlowDetails = useSelector(state =>
     LoadingSelectors.getLoading(state, 'fetchRegistryFlowDetails')
   );
+
   return (
     <div>
       <FullPageLoader
@@ -548,8 +550,12 @@ function DeployPage() {
                     <CheckboxField
                       name="check"
                       label="Keep existing Parameter Contexts"
-                      checked={keepParameter}
-                      onChange={e => setKeepParameter(e.target.checked)}
+                      checked={keepParameters}
+                      onChange={e => {
+                        dispatch(
+                          NamespacesActions.setKeepParameters(e.target.checked)
+                        );
+                      }}
                     />
                   </div>
                 </div>
