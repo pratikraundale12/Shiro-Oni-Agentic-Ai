2c0661212790bccd25c771f9c207ed07b3f37ab8
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-07-25 15:45:17 +0530

    copy cluster option removed

--- src/pages/Clusters/ListClusters.jsx
+++ src/pages/Clusters/ListClusters.jsx
@@ -240,10 +240,10 @@ export const ListClusters = () => {
                         <span>{KDFM.EDIT}</span>
                       </Item>
                     )}
-                    <Item onClick={handleCopyClusterClick}>
+                    {/* <Item onClick={handleCopyClusterClick}>
                       <CopyIcon width={16} height={16} />
                       <span>Copy Cluster</span>
-                    </Item>
+                    </Item> */}
                     <>
                       {item.status !== CLUSTER_STATUS.DISCONNECTED && (
                         <Item onClick={() => handleClick('view')}>
@@ -381,21 +381,21 @@ export const ListClusters = () => {
     GridSelectors.getGridData(state, 'clusters')
   );
 
-  const handleCopyClusterClick = () => {
-    const copiedData = {
-      ...menuState.row,
-      id: undefined,
-      name: getNextUniqueName(menuState?.row?.name, gridData),
-    };
-    dispatch(
-      ClustersActions.setCopyClusterData({
-        data: copiedData,
-        originalName: menuState.row.name,
-      })
-    );
-    dispatch(ClustersActions.setCopyClusterModalOpen(true));
-    handleCloseMenu();
-  };
+  // const handleCopyClusterClick = () => {
+  //   const copiedData = {
+  //     ...menuState.row,
+  //     id: undefined,
+  //     name: getNextUniqueName(menuState?.row?.name, gridData),
+  //   };
+  //   dispatch(
+  //     ClustersActions.setCopyClusterData({
+  //       data: copiedData,
+  //       originalName: menuState.row.name,
+  //     })
+  //   );
+  //   dispatch(ClustersActions.setCopyClusterModalOpen(true));
+  //   handleCloseMenu();
+  // };
 
   const handleCopyClusterConfirm = () => {
     history.push('/clusters/add', { state: copyClusterData });
