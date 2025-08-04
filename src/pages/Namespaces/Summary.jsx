0c81d3ae856bd96105edc81c413cf24c15efc0a3
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-08-04 23:51:05 +0530

    registry changes

--- src/pages/Namespaces/Summary.jsx
+++ src/pages/Namespaces/Summary.jsx
@@ -533,6 +533,22 @@ const Summary = () => {
   const isRegistryDeploy = useSelector(
     NamespacesSelectors.getdeployRegistryFlow
   );
+
+  const registryDropdownOptions = registryData.map(item => ({
+      label: item?.name,
+      value: item?.nifiRegistryId,
+      default_registry_id: item?.is_default,
+    }));
+    console.log(registryDropdownOptions?.[0]?.value, 'registryDropdownOptions');
+    console.log(registryData,'registryData');
+    
+    
+ const defaultRegistry = registryDropdownOptions.find(
+      item => item.default_registry_id === true
+    );
+ const defaultRegistryValue = defaultRegistry?.value || '';
+ console.log(defaultRegistryValue,'defaultRegistryValue');
+ 
   const XcordUpdated = useSelector(NamespacesSelectors.getregistryFlowXCord);
   const YcordUpdated = useSelector(NamespacesSelectors.getregistryFlowYCord);
   const registryDetailsData = useSelector(
@@ -1040,7 +1056,7 @@ const Summary = () => {
       version: registryFlowVerion?.version,
       flowId: registryFlowVerion?.flowId,
       bucketId: registryFlowVerion?.bucketId,
-      registryId: registrySelectedId || registryData?.id,
+      registryId: registrySelectedId || registryData?.id || defaultRegistryValue || registryDropdownOptions?.[0]?.value,
       flowName: formDataRegistry?.selectedFlowName,
       namespaceStatus: flowControlState,
       position: {
@@ -1072,7 +1088,7 @@ const Summary = () => {
       version: versionSelected?.version,
       namespaceId: checkDestCluster?.id,
       bucketId: checkDestCluster?.bucketId,
-      registryId: registrySelectedId || registryData?.id,
+      registryId: registrySelectedId || registryData?.id || registryDropdownOptions?.[0]?.value,
       namespaceStatus: flowControlState,
       payload: {
         namespaceId: checkDestCluster?.value,
@@ -1107,7 +1123,7 @@ const Summary = () => {
       version: registryFlowVerion?.version,
       flowId: registryFlowVerion?.flowId,
       bucketId: registryFlowVerion?.bucketId,
-      registryId: registrySelectedId || registryData?.id,
+      registryId: registrySelectedId || registryData?.id || defaultRegistryValue || registryDropdownOptions?.[0]?.value,
       namespaceId: checkDestCluster?.value,
       mode: 'deploy',
       scheduledTime: timeDeployScheduleDeployment?.toISOString(),
@@ -1150,7 +1166,7 @@ const Summary = () => {
         version: registryFlowVerion?.version,
         flowId: registryFlowVerion?.flowId,
         bucketId: registryFlowVerion?.bucketId,
-        registryId: registrySelectedId || registryData?.id,
+        registryId: registrySelectedId || registryData?.id || defaultRegistryValue || registryDropdownOptions?.[0]?.value,
         namespaceId: checkDestCluster?.value,
         mode: 'deploy',
         scheduledTime: timeDeployScheduleDeployment?.toISOString(),
@@ -1189,7 +1205,7 @@ const Summary = () => {
         version: versionSelected?.version,
        flowId: selectedNameSpace?.flowId || singleNamespaceData1?.flowId,
       namespaceId: checkDestCluster?.id || singleNamespaceData1?.id,
-      registryId: registrySelectedId || registryData?.id || singleNamespaceData1?.registryId,
+      registryId: registrySelectedId || registryData?.id || singleNamespaceData1?.registryId || defaultRegistryValue || registryDropdownOptions?.[0]?.value,
       bucketId: selectedNameSpace?.bucketId || singleNamespaceData1?.bucketId,
         namespaceStatus: flowControlSelectedScheduleStored || scheduleFlowType,
         payload: {
@@ -1250,7 +1266,7 @@ const Summary = () => {
       version: versionSelected?.version,
       flowId: selectedNameSpace?.flowId || singleNamespaceData1?.flowId,
       namespaceId: checkDestCluster?.id || singleNamespaceData1?.id,
-      registryId: registryData?.id || singleNamespaceData1?.registryId,
+      registryId: registryData?.id || singleNamespaceData1?.registryId || defaultRegistryValue || registryDropdownOptions?.[0]?.value,
       bucketId: selectedNameSpace?.bucketId || singleNamespaceData1?.bucketId,
       namespaceStatus: flowControlSelectedScheduleStored || scheduleFlowType,
       revert_local_changes: shouldRevertChanges,
