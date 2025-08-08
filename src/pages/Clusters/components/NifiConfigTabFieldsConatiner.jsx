3216969d3dda5d7a122e6de1db7e9f52127d35e8
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-08-08 10:33:29 +0530

    config edit functionality

--- src/pages/Clusters/components/NifiConfigTabFieldsConatiner.jsx
+++ src/pages/Clusters/components/NifiConfigTabFieldsConatiner.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { InputField, RadioSelectField } from '../../../shared';
+import { InputField } from '../../../shared';
 import { TRUE_FALSE_OPTIONS } from '../../../constants';
 import { NotePadIcon } from '../../../assets';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
 import styled from 'styled-components';
+import RadioSelectFieldWithWatch from '../../../shared/FormInputs/components/RadioSelectFieldWithWatch';
 
 const NoDataText = styled.div`
   color: ${props => props.theme.colors.lightGrey3};
@@ -17,6 +18,7 @@ const NifiConfigTabFieldsContainer = ({
   register,
   errors,
   allNifiProperties,
+  watch,
 }) => {
   const inputStringFields = allNifiProperties?.nifi_properties?.filter(
     ele =>
@@ -59,12 +61,13 @@ const NifiConfigTabFieldsContainer = ({
         ))}
         {topInputBooleanFields?.map(radio => (
           <div className="col-6" key={radio?.name}>
-            <RadioSelectField
+            <RadioSelectFieldWithWatch
               name={radio?.name}
               options={TRUE_FALSE_OPTIONS}
               label={radio?.label}
               register={register}
               defaultValue={'false'}
+              watch={watch}
             />
           </div>
         ))}
@@ -85,12 +88,13 @@ const NifiConfigTabFieldsContainer = ({
         ))}
         {inputBooleanFields?.map(radio => (
           <div className="col-5" key={radio?.name}>
-            <RadioSelectField
+            <RadioSelectFieldWithWatch
               name={radio?.name}
               options={TRUE_FALSE_OPTIONS}
               label={radio?.label}
               register={register}
               defaultValue={'false'}
+              watch={watch}
             />
           </div>
         ))}
---
+++ src/shared/FormInputs/components/RadioSelectFieldWithWatch/index.jsx
@@ -0,0 +1,57 @@
+import { isEmpty } from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+
+import FieldErrorMessage from '../FieldErrorMessage';
+import RadioField from '../RadioField';
+
+const Label = styled.h3`
+  font-size: 14px;
+  font-weight: 500;
+  text-align: left;
+  color: ${props => props.theme.colors.darker};
+  margin-bottom: 0.5rem;
+`;
+
+export const RadioSelectFieldWithWatch = ({
+  label,
+  register,
+  name,
+  options,
+  errors = {},
+  watch,
+  ...props
+}) => {
+  return (
+    <>
+      <Label>{label}</Label>
+      <div className="d-flex mb-2">
+        {options.map(option => (
+          <div key={option.label} className="mr-2">
+            <RadioField
+              name={name}
+              label={option.label}
+              value={option.value}
+              register={register}
+              checked={watch(name) === option.value}
+              {...props}
+            />
+          </div>
+        ))}
+      </div>
+      {!isEmpty(errors) && <FieldErrorMessage name={name} errors={errors} />}
+    </>
+  );
+};
+
+RadioSelectFieldWithWatch.propTypes = {
+  name: PropTypes.string.isRequired,
+  register: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  errors: PropTypes.shape({}),
+  watch: PropTypes.any,
+};
+
+export default RadioSelectFieldWithWatch;
