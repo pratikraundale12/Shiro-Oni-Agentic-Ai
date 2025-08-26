1697faa842c8d1353b6f4268f9c6c2875afe202c
NishantPatelKsolves <nishant.patel@ksolves.com>
2025-08-26 15:16:59 +0530

    update icon in cluster and roles page

--- src/assets/Icons/LoginIcon.jsx
+++ src/assets/Icons/LoginIcon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const LoginIcon = ({ width = 20, height = 20, color = '#0CBF59' }) => {
+export const LoginIcon = ({ width = 20, height = 20, color = '#06C270' }) => {
   return (
     <svg
       width={width}
@@ -10,26 +10,24 @@ export const LoginIcon = ({ width = 20, height = 20, color = '#0CBF59' }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g opacity="0.5">
-        <path
-          d="M5.93262 5.04016C6.13928 2.64016 7.37262 1.66016 10.0726 1.66016H10.1593C13.1393 1.66016 14.3326 2.85349 14.3326 5.83349V10.1802C14.3326 13.1602 13.1393 14.3535 10.1593 14.3535H10.0726C7.39262 14.3535 6.15928 13.3868 5.93928 11.0268"
-          stroke={color}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M1.33301 8H9.91967"
-          stroke={color}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M8.43262 5.7666L10.666 7.99994L8.43262 10.2333"
-          stroke={color}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
+      <path
+        d="M5.93164 5.04016C6.13831 2.64016 7.37164 1.66016 10.0716 1.66016H10.1583C13.1383 1.66016 14.3316 2.85349 14.3316 5.83349V10.1802C14.3316 13.1602 13.1383 14.3535 10.1583 14.3535H10.0716C7.39164 14.3535 6.15831 13.3868 5.93831 11.0268"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.33398 8H9.92065"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.43164 5.7666L10.665 7.99994L8.43164 10.2333"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
