cbdc6ebbba281eb3c1bd96ec3b1ab4169264abbe
NishantPatelKsolves <nishant.patel@ksolves.com>
2025-09-17 12:34:03 +0530

    show invalid icon on controller services

--- src/pages/Namespaces/ConfigDetailsPage.jsx
+++ src/pages/Namespaces/ConfigDetailsPage.jsx
@@ -120,6 +120,19 @@ const IconContentV2 = styled.div`
     stroke: rgba(255, 122, 0, 1);
   }
 `;
+
+const TabLabelWithIcon = styled.div`
+  position: relative;
+  display: inline-block;
+  padding-right: 16px; /* give space for the icon */
+`;
+
+const TopRightIcon = styled.div`
+  position: absolute;
+  top: -4px;
+  right: -4px;
+`;
+
 const ConfigDetailsPage = () => {
   const dispatch = useDispatch();
   const breadcrumbDataOnDeploy = [
@@ -447,7 +460,14 @@ const ConfigDetailsPage = () => {
                 }
               />
             </IconContentV2>
-            {KDFM.CONTROLLER_SERVICE}{' '}
+            <TabLabelWithIcon>
+              {KDFM.CONTROLLER_SERVICE}
+              {singleNameSpace?.invalidCount > 0 && (
+                <TopRightIcon>
+                  <InvalidProcessorIcon width={12} height={12} />
+                </TopRightIcon>
+              )}
+            </TabLabelWithIcon>
           </Tab>
         </TabWrapper>
         <TabContent>{renderContent()}</TabContent>
