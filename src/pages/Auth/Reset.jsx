f60712f66f977bdd74b14b32014b223a9deb5a63
naman-shukla-ksolves <naman.shukla@ksolves.com>
2025-09-24 18:30:00 +0530

    remove uppercase and number validation

--- src/pages/Auth/Reset.jsx
+++ src/pages/Auth/Reset.jsx
@@ -78,8 +78,6 @@ const validationSchema = yup.object().shape({
     .required('Password is required')
     .min(8, 'Password must be at least 8 characters')
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[0-9]/, 'Password must contain at least one number')
     .matches(
       /[@$!%*?&]/,
       'Password must contain at least one special character'
