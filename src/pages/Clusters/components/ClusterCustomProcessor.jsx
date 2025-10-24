dbf89558ac6efb4c2eb773f4fc9196b5d1851928
HemantSharmaKsolves <hemant.sharma@ksolves.com>
2025-10-24 17:37:10 +0530

    added deleted functionality in the nar and driver section

--- src/pages/Clusters/components/ClusterCustomProcessor.jsx
+++ src/pages/Clusters/components/ClusterCustomProcessor.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { Button } from '../../../shared';
+import { Button, ModalWithIcon } from '../../../shared';
 import PropTypes from 'prop-types';
 import { ClustersActions, ClustersSelectors } from '../../../store/clusters';
 import { isEmpty } from 'lodash';
@@ -11,8 +11,13 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { FullPageLoader, Table } from '../../../components';
 import PemUploadField from '../PEMUploadFile';
-import { DeleteSmallIcon } from '../../../assets';
+import {
+  DeleteDustbinIcon,
+  DeleteSmallIcon,
+  NotePadIcon,
+} from '../../../assets';
 import { LoadingSelectors } from '../../../store';
+import { theme } from '../../../styles';
 
 const Container = styled.div``;
 const ModalContainer = styled.div`
@@ -48,12 +53,22 @@ export const ClusterCustomProcessor = ({ data }) => {
   const dispatch = useDispatch();
   const narList = useSelector(ClustersSelectors.getnarList);
   const [fileInputKey, setFileInputKey] = useState(0);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [narSelected, setNarSelected] = useState({});
   const loading = useSelector(state =>
     LoadingSelectors.getLoading(state, 'addNarFile')
   );
   const loading2 = useSelector(state =>
     LoadingSelectors.getLoading(state, 'restartCluster')
   );
+
+  const loading3 = useSelector(state =>
+    LoadingSelectors.getLoading(state, 'fetchNarList')
+  );
+  const loading4 = useSelector(state =>
+    LoadingSelectors.getLoading(state, 'deleteClusterNarFile')
+  );
+
   const schema = yup.object().shape({
     nar_file: yup.mixed().required('File is required'),
   });
@@ -65,11 +80,34 @@ export const ClusterCustomProcessor = ({ data }) => {
   } = useForm({
     resolver: yupResolver(schema),
   });
+  const handleDeleteClick = item => {
+    setNarSelected(item);
+    setIsDeleteModalOpen(true);
+  };
+  const handleNarDeleteConfirm = () => {
+    dispatch(
+      ClustersActions.deleteClusterNarFile({
+        id: narSelected?.clusterId,
+        narId: narSelected?.id,
+      })
+    );
+    setNarSelected({});
+    setIsDeleteModalOpen(false);
+  };
 
   const COLUMNS = [
     {
       label: 'File Name',
-      renderCell: item => <>{item?.name || 'N/A'}</>,
+      renderCell: item => (
+        <>
+          {' '}
+          {item?.addedViaDFM && (
+            <NotePadIcon height="21" width="21" color={theme.colors.primary} />
+          )}{' '}
+          &nbsp;
+          {item?.name || 'N/A'}
+        </>
+      ),
       resize: true,
       width: '40%',
     },
@@ -85,8 +123,12 @@ export const ClusterCustomProcessor = ({ data }) => {
       renderCell: item => (
         <>
           {
-            <span data-tooltip-id={`certificate-${item?.id}-detail`}>
-              {false && <DeleteSmallIcon color="red" />}
+            <span
+              data-tooltip-id={`delete nar`}
+              onClick={() => handleDeleteClick(item)}
+              style={{ cursor: 'pointer' }}
+            >
+              {item?.addedViaDFM && <DeleteSmallIcon color="red" />}
             </span>
           }{' '}
         </>
@@ -119,7 +161,7 @@ export const ClusterCustomProcessor = ({ data }) => {
   };
   return (
     <>
-      <FullPageLoader loading={loading || loading2} />
+      <FullPageLoader loading={loading || loading2 || loading3 || loading4} />
 
       <Container>
         <div className="row mb-3">
@@ -160,9 +202,30 @@ export const ClusterCustomProcessor = ({ data }) => {
             </Button>
           </div>
         </FlexWrapper>
+        <div className="ms-3 mt-2 d-flex justify-content-end">
+          <div>
+            <div style={{ fontSize: '14px', fontWeight: '600' }}>Legend</div>
+            <NotePadIcon
+              height="21"
+              width="21"
+              color={theme.colors.primary}
+            />{' '}
+            : Added by DFM &nbsp;&nbsp;
+          </div>
+        </div>
         <div className="mt-2">
           <Table data={narList || []} columns={COLUMNS} />
         </div>
+        <ModalWithIcon
+          title={`Delete Nar`}
+          primaryButtonText={'Delete'}
+          secondaryButtonText="Cancel"
+          icon={<DeleteDustbinIcon />}
+          isOpen={isDeleteModalOpen}
+          onRequestClose={() => setIsDeleteModalOpen(false)}
+          primaryText={`Are you sure you want to delete?`}
+          onSubmit={handleNarDeleteConfirm}
+        />
       </Container>
     </>
   );
--- src/store/clusters/redux.js
+++ src/store/clusters/redux.js
@@ -180,6 +180,8 @@ export const ClustersActions = {
   deleteClusterKube: createAction(`${prefix}deleteClusterKube`),
   setTourIndex: createAction(`${prefix}setTourIndex`),
   setTourStart: createAction(`${prefix}setTourStart`),
+  deleteClusterNarFile: createAction(`${prefix}deleteClusterNarFile`),
+  deleteClusterDriverFile: createAction(`${prefix}deleteClusterDriverFile`),
 };
 
 /* ------------- INITIAL STATE ------------- */
@@ -707,7 +709,6 @@ const setTourStart = (state, { payload }) => {
     tourStart: payload,
   };
 };
-
 /* ------------- Hookup Reducers To Types ------------- */
 export const clustersReducer = createReducer(
   CLUSTERS_INITIAL_STATE,
