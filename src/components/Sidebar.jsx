e7a90c3da3e3c16937605049baaf94e3e57f4b44
NishantPatelKsolves <nishant.patel@ksolves.com>
2025-11-30 13:08:39 +0530

    update app version to 2.1.21-3

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -326,7 +326,7 @@ export const Sidebar = ({
 
       <KDFMVersion>
         {/* FIX_ME: Later will come from API */}
-        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.21-2`}</span>
+        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.21-3`}</span>
       </KDFMVersion>
     </Container>
   );
