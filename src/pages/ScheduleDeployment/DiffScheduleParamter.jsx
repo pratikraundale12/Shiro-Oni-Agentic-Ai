61cf69f5e128c292fe0772c701e1f5325923a8ce
Rahul Bhardwaj <rahul.bhardwaj@ksolves.com>
2025-12-16 22:59:09 +0530

    fixed eslint

--- src/pages/ScheduleDeployment/DiffScheduleParamter.jsx
+++ src/pages/ScheduleDeployment/DiffScheduleParamter.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { SchedularSelectors } from '../../store/schedular';
 import { useSelector } from 'react-redux';
-import { theme } from '../../styles';
 import { NoDataIcon } from '../../assets';
 import { isEmpty } from 'lodash';
 
