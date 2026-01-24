a67d93f5c315b533f9e5a1aecd03caefac6754da
NishantPatelKsolves <nishant.patel@ksolves.com>
2026-01-24 18:06:15 +0530

    pre-select cluster in login modal and create DFM links for cluster and PG

--- src/pages/AgenticAI/AgenticAiMessageList.jsx
+++ src/pages/AgenticAI/AgenticAiMessageList.jsx
@@ -301,7 +301,11 @@ export const AgenticAiMessageList = ({
                       >
                         {item.data}
                       </ReactMarkdown>
-                      {item.isLoginRequired && <AgenticAiClusterLoginButton />}
+                      {item.isLoginRequired && (
+                        <AgenticAiClusterLoginButton
+                          clusterId={item.clusterId}
+                        />
+                      )}
                       {item.isLogoutRequired && (
                         <AgenticAiClusterLogoutButton />
                       )}
