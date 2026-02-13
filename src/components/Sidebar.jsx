d559995defbf8976c0293dc6eb97c525818f0882
Rahul Bhardwaj <rahul.bhardwaj@ksolves.com>
2026-02-13 19:12:06 +0530

    changed 2Degrees release verfion to 2.1.21-4 for profiling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -326,7 +326,7 @@ export const Sidebar = ({
 
       <KDFMVersion>
         {/* FIX_ME: Later will come from API */}
-        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.21-3`}</span>
+        <span className="version-content">{`V${collapsed ? '' : 'ersion'} 2.1.21-4`}</span>
       </KDFMVersion>
     </Container>
   );
